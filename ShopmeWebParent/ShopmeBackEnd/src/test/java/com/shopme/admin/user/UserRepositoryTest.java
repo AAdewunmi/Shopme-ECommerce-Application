271346cbac54757887a6b94ea6224bf4f8f3e14e
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-09-04 12:45:08 +0100

    Twenty-Seven Commit: Implement testCreateUser() method

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTest.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTest.java
@@ -1,5 +1,31 @@
 package com.shopme.admin.user;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+
+@DataJpaTest(showSql = false)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
 public class UserRepositoryTest {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	@Test
+	public void testCreateUser() {
+		
+	}
+	
 
 }
