bb9db0359a001f4f8c32c560f4c31f38a685a336
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-09-04 13:12:37 +0100

    Twenty-Eight Commit: Implement overloaded constructor for User class

--- ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
@@ -48,6 +48,13 @@ public class User {
 	public User() {
 		super();
 	}
+	
+
+	public User(Integer id) {
+		super();
+		this.id = id;
+	}
+
 
 	public User(String email, String password, String firstName, String lastName) {
 		super();
