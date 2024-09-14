466d129517fca41ff0d9bb27efd3b66644c063cf
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-09-14 11:36:10 +0100

    Seventy Commit: Implement userDetailsService() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -2,11 +2,17 @@ package com.shopme.admin.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig {
+	
+	@Bean
+	UserDetailsService userDetailsService() {
+		return new ShopmeUserDetailsService();
+	}
 	
 	@Bean
 	PasswordEncoder passwordEncoder() {
