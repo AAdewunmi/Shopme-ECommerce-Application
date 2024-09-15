4a0f94a849145afc8c589acc03ddff185a271225
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-09-15 10:16:43 +0100

    Seventy-Seven Commit: Implement getUserByEmail() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
@@ -1,9 +1,12 @@
 package com.shopme.admin.user;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.shopme.admin.paging.SearchRepository;
 import com.shopme.common.entity.User;
 
-public interface UserRepository extends CrudRepository<User, Integer>{
-	
+public interface UserRepository extends SearchRepository<User, Integer>{
+	@Query("SELECT u FROM User u WHERE u.email = :email")
+	public User getUserByEmail(@Param("email") String email);
 }
