b85f90f90615775f61f2ae2c883c5961fe0be4c0
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-09-15 10:05:44 +0100

    Seventy-Five Commit: Create skeleton SearchRepository interface

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/paging/SearchRepository.java
@@ -0,0 +1,7 @@
+package com.shopme.admin.paging;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface SearchRepository extends PagingAndSortingRepository<T, ID> {
+
+}
