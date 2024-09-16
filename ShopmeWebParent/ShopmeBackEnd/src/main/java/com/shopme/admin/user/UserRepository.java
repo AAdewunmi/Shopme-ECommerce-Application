bc17c0829cfb62cd31079a9e9a1623804efce3b4
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-09-16 15:24:06 +0100

    Ninety-Two Commit: Implement findAll() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
@@ -1,12 +1,25 @@
 package com.shopme.admin.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.util.Streamable;
 
 import com.shopme.admin.paging.SearchRepository;
 import com.shopme.common.entity.User;
+import java.util.List;
+
 
 public interface UserRepository extends SearchRepository<User, Integer>{
 	@Query("SELECT u FROM User u WHERE u.email = :email")
 	public User getUserByEmail(@Param("email") String email);
+	
+	public Long countById(Integer id);
+	
+	@Query("SELECT u FROM User u WHERE CONCAT(u.id, ' ', u.email, ' ', u.firstName, ' ',"
+			+ " u.lastName) LIKE %?1%")
+	public Page<User> findAll(String keyword, Pageable pageable);
+	
 }
