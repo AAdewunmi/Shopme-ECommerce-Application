cd802ffc15ce1ea175dfa0dbf9d1bdef1c25dc20
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-09-18 11:11:58 +0100

    Ninety-Four Commit: Implement editUser() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -41,6 +42,12 @@ public class UserController {
 		service.save(user);
 		redirectAttributes.addFlashAttribute("message", "User has been Created!");
 		return "redirect:/users";
+	}
+	
+	@GetMapping("/users/edit/{id}")
+	public String editUser(@PathVariable(name = "id") Integer id) {
+		User user = service.get(id);
+		return "user_form";
 	}
 
 }
