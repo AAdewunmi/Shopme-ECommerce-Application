c42fe35adc0e17ce7619f3a660556b21333b527d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-09-18 11:49:09 +0100

    Ninety-Five Commit: Refactor UserRepository class

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
@@ -1,25 +1,11 @@
 package com.shopme.admin.user;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort.Order;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.util.Streamable;
-
-import com.shopme.admin.paging.SearchRepository;
 import com.shopme.common.entity.User;
-import java.util.List;
 
-
-public interface UserRepository extends SearchRepository<User, Integer>{
+public interface UserRepository extends CrudRepository<User, Integer>{
 	@Query("SELECT u FROM User u WHERE u.email = :email")
 	public User getUserByEmail(@Param("email") String email);
-	
-	public Long countById(Integer id);
-	
-	@Query("SELECT u FROM User u WHERE CONCAT(u.id, ' ', u.email, ' ', u.firstName, ' ',"
-			+ " u.lastName) LIKE %?1%")
-	public Page<User> findAll(String keyword, Pageable pageable);
-	
 }
