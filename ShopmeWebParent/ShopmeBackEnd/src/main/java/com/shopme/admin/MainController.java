f781cacc6ef2b7a1c5bfbb7333229ccef103f911
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-09-19 11:29:57 +0100

    One Hundred One Commit: Fix Error "The page isn’t redirecting properly"

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 	
-	@GetMapping("")
+	@GetMapping("/login")
 	public String viewHomePage() {
-		return "index";
+		return "login";
 	}
 
 }
