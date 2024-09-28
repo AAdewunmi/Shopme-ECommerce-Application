c05e30cfbc6d3f1659ba3795442802593348e9d5
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-09-28 11:34:04 +0100

    One Hundred Thirty One Commit: Implement saveFile() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/FileUploadUtil.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/FileUploadUtil.java
@@ -1,5 +1,34 @@
 package com.shopme.admin;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
+
 public class FileUploadUtil {
+	
+private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadUtil.class);
+	
+	public static void saveFile(String uploadDir, String fileName, 
+			MultipartFile multipartFile) throws IOException {
+		Path uploadPath = Paths.get(uploadDir);
+		
+		if (!Files.exists(uploadPath)) {
+			Files.createDirectories(uploadPath);
+		}
+		
+		try (InputStream inputStream = multipartFile.getInputStream()) {
+			Path filePath = uploadPath.resolve(fileName);
+			Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException ex) {
+			throw new IOException("Could not save file: " + fileName, ex);
+		}
+	}
 
 }
