39f432ee83ce5716bb4d3a162126c53675f6372b
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-10-03 11:20:20 +0100

    One Hundred Forty Six Commit: Implement listByPage() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -1,10 +1,13 @@
 package com.shopme.admin.user;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +19,7 @@ import jakarta.transaction.Transactional;
 @Service
 @Transactional
 public class UserService {
-	
+	public static final int USERS_PER_PAGE = 4;
 	@Autowired
 	private UserRepository userRepo;
 	
@@ -28,6 +31,11 @@ public class UserService {
 	
 	public List<User> listAll(){
 		return (List<User>) userRepo.findAll();
+	}
+	
+	public Page<User> listByPage(int pageNum){
+		PageRequest pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE);
+		return userRepo.findAll(pageable);
 	}
 	
 	public List<Role> listRoles(){
