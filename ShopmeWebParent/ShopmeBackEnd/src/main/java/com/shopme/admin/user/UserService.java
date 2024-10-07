76a8bd51d3e0b87408a464d0888ec1939b25c6f7
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-10-07 10:02:34 +0100

    One Hundred Fifty Seven Commit: Refactor listByPage() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -4,10 +4,12 @@ import java.awt.print.Pageable;
 import java.util.List;
 
 import java.util.NoSuchElementException;
+import java.util.SortedMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -33,8 +35,10 @@ public class UserService {
 		return (List<User>) userRepo.findAll();
 	}
 	
-	public Page<User> listByPage(int pageNum){
-		PageRequest pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE);
+	public Page<User> listByPage(int pageNum, String sortField, String sortDir){
+		Sort sort = Sort.by(sortField);
+		sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+		PageRequest pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE, sort);
 		return userRepo.findAll(pageable);
 	}
 	
