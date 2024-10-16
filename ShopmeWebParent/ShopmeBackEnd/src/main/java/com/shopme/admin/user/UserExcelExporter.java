43d0d25766491568b0967f2572d2ec7df93d8819
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-10-16 07:07:29 +0100

    One Hundred Ninety Two Commit: Implement UserExcelExporter constructor method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserExcelExporter.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserExcelExporter.java
@@ -1,19 +1,73 @@
 package com.shopme.admin.user;
 
+import java.awt.Font;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import com.shopme.common.entity.User;
 
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class UserExcelExporter extends AbstractExporter{
+	private XSSFWorkbook workbook;
+	private XSSFSheet sheet;
+	
+	public UserExcelExporter() {
+		super();
+		this.workbook = new XSSFWorkbook();
+	}
+
+	private void writeHeaderLine() {
+		sheet = workbook.createSheet("Users");
+		XSSFRow row = sheet.createRow(0);
+		
+		XSSFCellStyle cellStyle = workbook.createCellStyle();
+		XSSFFont font = workbook.createFont();
+		font.setBold(true);
+		font.setFontHeight(16);
+		cellStyle.setFont(font);
+		
+		createCell(row, 0, "User Id", cellStyle);
+		createCell(row, 1, "E-mail", cellStyle);
+		createCell(row, 2, "First Name", cellStyle);
+		createCell(row, 3, "Last Name", cellStyle);
+		createCell(row, 4, "Roles", cellStyle);
+		createCell(row, 5, "Enabled", cellStyle);
+		
+	}
+	
+	private void createCell() {
+		
+	}
 	
 	public void export(List<User> listUsers, HttpServletResponse response) throws IOException {
-		super.setResponseHeader(response, "application/octet-stream", ".xslx");
+		super.setResponseHeader(response, "application/octet-stream", ".xlsx");
+		XSSFWorkbook workbook = new XSSFWorkbook();
+		XSSFSheet sheet = workbook.createSheet("Users");
+		XSSFRow row = sheet.createRow(0);
+		XSSFCellStyle cellStyle = workbook.createCellStyle();
+		XSSFFont font = workbook.createFont();
+		font.setBold(true);
+		font.setFontHeight(16);
+		cellStyle.setFont(font);
+		XSSFCell cell = row.createCell(0);
+		cell.setCellValue("User ID");
+		cell.setCellStyle(cellStyle);
+		ServletOutputStream outputStream = response.getOutputStream();
+		workbook.write(outputStream);
+		workbook.close();
+		outputStream.close();
 	}
 
 }
