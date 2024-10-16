399ca87e3f6910f2b1cecdf06a06232a92a9b881
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-10-16 07:22:04 +0100

    One Hundred Ninety Four Commit: Implement writeHeaderLine() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserExcelExporter.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserExcelExporter.java
@@ -30,7 +30,19 @@ public class UserExcelExporter extends AbstractExporter{
 	}
 
 	private void writeHeaderLine() {
-		
+		sheet = workbook.createSheet("Users");
+		XSSFRow row = sheet.createRow(0);
+		XSSFCellStyle cellStyle = workbook.createCellStyle();
+		XSSFFont font = workbook.createFont();
+		font.setBold(true);
+		font.setFontHeight(16);
+		cellStyle.setFont(font);
+		createCell(row, 0, "User Id", cellStyle);
+		createCell(row, 1, "E-mail", cellStyle);
+		createCell(row, 2, "First Name", cellStyle);
+		createCell(row, 3, "Last Name", cellStyle);
+		createCell(row, 4, "Roles", cellStyle);
+		createCell(row, 5, "Enabled", cellStyle);
 	}
 	
 	private void createCell(XSSFRow row, int columnIndex, Object value, CellStyle style) {
@@ -50,17 +62,6 @@ public class UserExcelExporter extends AbstractExporter{
 	
 	public void export(List<User> listUsers, HttpServletResponse response) throws IOException {
 		super.setResponseHeader(response, "application/octet-stream", ".xlsx");
-		XSSFWorkbook workbook = new XSSFWorkbook();
-		XSSFSheet sheet = workbook.createSheet("Users");
-		XSSFRow row = sheet.createRow(0);
-		XSSFCellStyle cellStyle = workbook.createCellStyle();
-		XSSFFont font = workbook.createFont();
-		font.setBold(true);
-		font.setFontHeight(16);
-		cellStyle.setFont(font);
-		XSSFCell cell = row.createCell(0);
-		cell.setCellValue("User ID");
-		cell.setCellStyle(cellStyle);
 		ServletOutputStream outputStream = response.getOutputStream();
 		workbook.write(outputStream);
 		workbook.close();
