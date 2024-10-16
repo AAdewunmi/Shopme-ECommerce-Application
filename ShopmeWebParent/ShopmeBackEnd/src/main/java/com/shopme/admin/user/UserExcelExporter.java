6cc1ea25175e8caefccc4967b61d92875d1558df
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-10-16 07:10:57 +0100

    One Hundred Ninety Three Commit: Implement createCell() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserExcelExporter.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserExcelExporter.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFFont;
@@ -29,26 +30,22 @@ public class UserExcelExporter extends AbstractExporter{
 	}
 
 	private void writeHeaderLine() {
-		sheet = workbook.createSheet("Users");
-		XSSFRow row = sheet.createRow(0);
-		
-		XSSFCellStyle cellStyle = workbook.createCellStyle();
-		XSSFFont font = workbook.createFont();
-		font.setBold(true);
-		font.setFontHeight(16);
-		cellStyle.setFont(font);
-		
-		createCell(row, 0, "User Id", cellStyle);
-		createCell(row, 1, "E-mail", cellStyle);
-		createCell(row, 2, "First Name", cellStyle);
-		createCell(row, 3, "Last Name", cellStyle);
-		createCell(row, 4, "Roles", cellStyle);
-		createCell(row, 5, "Enabled", cellStyle);
 		
 	}
 	
-	private void createCell() {
+	private void createCell(XSSFRow row, int columnIndex, Object value, CellStyle style) {
+		XSSFCell cell = row.createCell(columnIndex);
+		sheet.autoSizeColumn(columnIndex);
 		
+		if (value instanceof Integer) {
+			cell.setCellValue((Integer) value);
+		} else if (value instanceof Boolean) {
+			cell.setCellValue((Boolean) value);
+		} else {
+			cell.setCellValue((String) value);
+		}
+		
+		cell.setCellStyle(style);		
 	}
 	
 	public void export(List<User> listUsers, HttpServletResponse response) throws IOException {
