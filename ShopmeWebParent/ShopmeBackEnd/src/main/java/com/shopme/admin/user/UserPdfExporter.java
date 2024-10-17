66ae0371cd2c3df067437f764c1f88794c50fbcc
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-10-17 14:02:56 +0100

    Two Hundred One Commit: Implement UserPdfExporter() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserPdfExporter.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserPdfExporter.java
@@ -1,5 +1,54 @@
 package com.shopme.admin.user;
 
-public class UserPdfExporter {
+import java.awt.Color;
+import java.io.IOException;
+import java.util.List;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+import com.shopme.common.entity.User;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+public class UserPdfExporter extends AbstractExporter{
+
+	public void export(List<User> listUsers, HttpServletResponse response) 
+		throws IOException{
+		super.setResponseHeader(response, "application/pdf", ".pdf");
+		
+		Document document = new Document(PageSize.A4);
+		PdfWriter.getInstance(document, response.getOutputStream());
+		
+		document.open();
+		
+		Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+		font.setSize(18);
+		font.setColor(Color.BLUE);
+		
+		Paragraph paragraph = new Paragraph("List of User", font);
+		paragraph.setAlignment(Paragraph.ALIGN_CENTER);
+		
+		document.add(paragraph);
+		
+		PdfPTable table = new PdfPTable(6);
+		table.setWidthPercentage(100f);
+		table.setSpacingBefore(10);
+		table.setWidths(new float[] {1.2f, 3.5f, 3.0f, 3.0f, 3.0f, 1.7f});
+		
+		//writeTableHeader(table);
+		//writeTableData(table, listUsers);
+		
+		document.add(table);
+		
+		document.close();
+		
+	}
 
 }
