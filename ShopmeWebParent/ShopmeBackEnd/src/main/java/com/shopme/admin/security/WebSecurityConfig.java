b0031fc65fb859cadae22232a643358ba015c47e
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-10-21 11:45:35 +0100

    Two Hundred Eighty Eight Commit: Implement authenticationProvider() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.shopme.admin.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,6 +20,13 @@ public class WebSecurityConfig {
 	@Bean 
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean DaoAuthenticationProvider authenticationProvider() {
+		 DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		 authProvider.setUserDetailsService(userDetailsService());
+		 authProvider.setPasswordEncoder(passwordEncoder()); 
+		 return authProvider; 
 	}
 
 }
