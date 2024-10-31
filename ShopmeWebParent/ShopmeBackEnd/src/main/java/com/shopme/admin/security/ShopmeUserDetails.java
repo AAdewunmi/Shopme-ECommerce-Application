50ca96d401b40de1c664b0303d01f3dafd9a65f5
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-10-31 08:17:36 +0000

    Three Hundred Thirty One Commit: Implement setFirstName() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
@@ -67,4 +67,8 @@ public class ShopmeUserDetails implements UserDetails {
 	public String getFullname() {
 		return this.user.getFirstName() + " " + this.user.getLastName();
 	}
+	
+	public void setFirstName(String firstName) {
+		this.user.setFirstName(firstName);
+	}
 }
