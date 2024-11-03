08a34744f7d3adcf0d028ce09234e0d04afcf03c
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-11-03 09:33:34 +0000

    Three Hundred Forty Three Commit: Add js code fragment from account_form.html

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form.js
@@ -1,22 +1,25 @@
-$(document).ready(function(){
-	$("#logoutLink").on("click", function(e){
-		e.preventDefault();
-		document.logoutForm.submit();
+$(document).ready(function() {
+		$("#buttonCancel").on("click", function() {
+			window.location = "[[@{/users}]]";
+		});
+		
+		$("#fileImage").change(function(){
+			fileSize = this.files[0].size;
+			if(fileSize > 1048576){
+				this.setCustomValidity("You must choose an image less than 1MB!");
+				this.reportValidity();
+			} else {
+				this.setCustomValidity("");
+				showImageThumbnail(this);
+			}
+		});
 	});
-	customizeDropDownMenu() 
-});
-
-function customizeDropDownMenu() {
-	$(".navbar .dropdown").hover(
-		function() {
-			$(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
-		},
-		function() {
-			$(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
-		}
-	);
-
-	$(".dropdown > a").click(function() {
-		location.href = this.href;
-	});
+	
+function showImageThumbnail(fileInput){
+	var file = fileInput.files[0];
+	var reader = new FileReader();
+	reader.onload = function(e){
+		$("#thumbnail").attr("src", e.target.result);
+	};
+	reader.readAsDataURL(file);
 }
