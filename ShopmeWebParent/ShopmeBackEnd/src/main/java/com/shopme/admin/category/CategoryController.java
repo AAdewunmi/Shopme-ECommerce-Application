763c4204def881279fe1627da8faaef6daa8a67a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-11-10 09:18:36 +0000

    Three Hundred Eighty Five Commit: Fix error during template parsing categories.html

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
@@ -5,15 +5,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.shopme.common.entity.Category;
 
+
 @Controller
+@CrossOrigin
 public class CategoryController {
 	
 	@Autowired
 	private CategoryService service;
+	
 	
 	@GetMapping("/categories")
 	public String listAll(Model model) {
