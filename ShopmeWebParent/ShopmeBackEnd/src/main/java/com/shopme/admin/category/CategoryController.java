a873361dfc410fc38953b1e7f7218c5d700770b9
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-11-13 16:28:35 +0000

    Four Hundred Four Commit: Refactor saveCategory() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.admin.FileUploadUtil;
 import com.shopme.common.entity.Category;
@@ -42,12 +43,14 @@ public class CategoryController {
 	}
 	
 	@PostMapping("/categories/save")
-	public String saveCategory(Category category, @RequestParam("fileImage") MultipartFile multipartFile) throws IOException{
+	public String saveCategory(Category category, @RequestParam("fileImage") MultipartFile multipartFile,
+			RedirectAttributes redirectAttributes) throws IOException{
 		String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
 		category.setImage(fileName);
 		Category savedCategory = service.save(category);
 		String uploadDir = "../category-images/" + savedCategory.getId();
 		FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+		redirectAttributes.addFlashAttribute("message", "The category has been saved successfully!");
 		return "redirect:/categories";
 	}
 }
