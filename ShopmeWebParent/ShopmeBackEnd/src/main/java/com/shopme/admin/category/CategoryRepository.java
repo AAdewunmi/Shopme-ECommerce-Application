bea8a63102b78d67db3b41fb86b6455941c793f6
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-11-26 07:23:52 +0000

    Four Hundred Sixty Seven Commit: Declare findRootCategories() abstract method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryRepository.java
@@ -3,6 +3,7 @@ package com.shopme.admin.category;
 import java.util.List;
 import java.util.function.LongToDoubleFunction;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,6 +15,9 @@ public interface CategoryRepository extends CrudRepository<Category, Integer> {
 	
 	@Query("SELECT c FROM Category c WHERE c.parent.id is NULL")
 	public List<Category> findRootCategories(Sort sort);
+	
+	@Query("SELECT c FROM Category c WHERE c.parent.id is NULL")
+	public List<Category> findRootCategories(Pageable pageable);
 	
 	public Long countById(Integer id);
 	
