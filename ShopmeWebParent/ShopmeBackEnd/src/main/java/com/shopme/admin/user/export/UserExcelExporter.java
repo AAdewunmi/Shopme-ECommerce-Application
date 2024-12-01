9f88d0fb9d4c4cee074c14230c2a2c8acae23a86
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-12-01 08:17:38 +0000

    Four Hundred Ninety Two Commit: Refactor export() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/export/UserExcelExporter.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/export/UserExcelExporter.java
@@ -15,6 +15,7 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import com.shopme.admin.AbstractExporter;
 import com.shopme.common.entity.User;
 
 import jakarta.servlet.ServletOutputStream;
@@ -61,7 +62,7 @@ public class UserExcelExporter extends AbstractExporter{
 	}
 	
 	public void export(List<User> listUsers, HttpServletResponse response) throws IOException {
-		super.setResponseHeader(response, "application/octet-stream", ".xlsx");
+		super.setResponseHeader(response, "application/octet-stream", ".xlsx", "users_");
 		writeHeaderLine();
 		writeDataLines(listUsers);
 		ServletOutputStream outputStream = response.getOutputStream();
