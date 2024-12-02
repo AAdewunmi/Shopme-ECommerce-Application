0cb2211d1fc25956233075ef7dc6d5faf46fa57f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-12-02 10:34:12 +0000

    Five Hundred Two Commit: Implement toString() method

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Brand.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Brand.java
@@ -80,4 +80,8 @@ public class Brand {
 		this.categories = categories;
 	}
 	
+	@Override
+	public String toString() {
+		return "Brand [id=" + id + ", name=" + name + ", categories=" + categories + "]";
+	}
 }
