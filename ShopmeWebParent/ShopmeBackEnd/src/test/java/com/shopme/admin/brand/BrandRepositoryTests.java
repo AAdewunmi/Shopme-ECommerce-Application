4250c697d386de27781f30400abf5e41a045888f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-12-02 10:31:59 +0000

    Five Hundred One Commit: Implement testCreateBrand1() method

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/brand/BrandRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/brand/BrandRepositoryTests.java
@@ -1,5 +1,35 @@
 package com.shopme.admin.brand;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Brand;
+import com.shopme.common.entity.Category;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
 public class BrandRepositoryTests {
+	
+	@Autowired
+	private BrandRepository repository;
+	
+	@Test
+	public void testCreateBrand1() {
+		Category laptops = new Category(6);
+		Brand acer = new Brand("Acer");
+		acer.getCategories().add(laptops);
+		
+		Brand savedBrand = repository.save(acer);
+		
+		assertThat(savedBrand).isNotNull();
+		assertThat(savedBrand.getId()).isGreaterThan(0);
+	}
 
 }
