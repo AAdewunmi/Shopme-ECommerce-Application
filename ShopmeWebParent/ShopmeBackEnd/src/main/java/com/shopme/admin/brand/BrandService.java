7c5773cbbd113831800d4aae7af2cb951154d07f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-12-03 07:42:14 +0000

    Five Hundred Five Commit: Implement save() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandService.java
@@ -1,5 +1,20 @@
 package com.shopme.admin.brand;
 
-public class BrandService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.shopme.common.entity.Brand;
+
+@Service
+public class BrandService {
+	
+	public static final int BRANDS_PER_PAGE = 10;
+	
+	@Autowired
+	private BrandRepository repository;
+	
+	public Brand save(Brand brand) {
+		return repository.save(brand);
+	}
+	
 }
