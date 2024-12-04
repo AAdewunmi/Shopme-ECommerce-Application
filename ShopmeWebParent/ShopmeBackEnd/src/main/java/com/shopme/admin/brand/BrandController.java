6cec67d134edd46b3bf59ebcb8ca6ac73919ed72
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-12-04 09:02:05 +0000

    Five Hundred Fourteen Commit: Implement listAll() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandController.java
@@ -1,5 +1,27 @@
 package com.shopme.admin.brand;
 
-public class BrandController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.shopme.common.entity.Brand;
+
+@Controller
+@CrossOrigin
+public class BrandController {
+	
+	@Autowired
+	private BrandService service;
+	
+	@GetMapping("/brands")
+	public String listAll(Model model) {
+		List<Brand> listBrands = service.listAll();
+		model.addAttribute("listBrands", listBrands);
+		return "brands/brands";
+	}
+	
 }
