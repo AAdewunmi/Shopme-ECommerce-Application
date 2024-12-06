ce205b89284bc1271a2f247cb0865646d26335f4
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-12-06 10:59:03 +0000

    Five Hundred Twenty Six Commit: Declare abstract method countById()

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
@@ -1,9 +1,8 @@
 package com.shopme.admin.brand;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
 import com.shopme.common.entity.Brand;
-public interface BrandRepository extends CrudRepository<Brand, Integer>{
 
+public interface BrandRepository extends CrudRepository<Brand, Integer>{
+	public Long countById(Integer id);
 }
