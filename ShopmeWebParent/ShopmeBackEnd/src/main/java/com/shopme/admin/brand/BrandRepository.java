7d1c01df056e0841a70b711bc3d0d9dbf2e834a1
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-12-08 13:03:05 +0000

    Five Hundred Thirty Five Commit: Declare findByName abstract method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
@@ -2,7 +2,13 @@ package com.shopme.admin.brand;
 
 import org.springframework.data.repository.CrudRepository;
 import com.shopme.common.entity.Brand;
+import java.util.List;
+
 
 public interface BrandRepository extends CrudRepository<Brand, Integer>{
+	
 	public Long countById(Integer id);
+	
+	public Brand findByName(String name);
+	
 }
