d27b1aa6a5c7a8f146861e0c6fbcb48884cb8021
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-12-10 14:35:52 +0000

    Five Hundred Forty Eight Commit: Implement listByPage() method in BrandService class

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandService.java
@@ -5,6 +5,10 @@ import java.util.NoSuchElementException;
 import java.util.function.LongToDoubleFunction;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.shopme.common.entity.Brand;
@@ -23,6 +27,20 @@ public class BrandService {
 	
 	public List<Brand> listAll(){
 		return (List<Brand>) repository.findAll();
+	}
+	
+	public Page<Brand> listByPage(int pageNum, String sortField, String sortDir, String keyword) {
+		Sort sort = Sort.by(sortField);
+		
+		sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+				
+		Pageable pageable = PageRequest.of(pageNum - 1, BRANDS_PER_PAGE, sort);
+		
+		if (keyword != null) {
+			return repository.findAll(keyword, pageable);
+		}
+		
+		return repository.findAll(pageable);		
 	}
 	
 	public Brand get(Integer id) throws BrandNotFoundException {
