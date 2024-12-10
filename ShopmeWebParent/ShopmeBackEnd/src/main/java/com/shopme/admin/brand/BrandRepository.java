65f9ef4de7e3842fa6204ba19111a541fd8e1432
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-12-10 09:26:06 +0000

    Five Hundred Forty Five Commit: Implement findAll() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
@@ -1,8 +1,11 @@
 package com.shopme.admin.brand;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.shopme.common.entity.Brand;
-import java.util.List;
+
 
 
 public interface BrandRepository extends CrudRepository<Brand, Integer>{
@@ -10,5 +13,8 @@ public interface BrandRepository extends CrudRepository<Brand, Integer>{
 	public Long countById(Integer id);
 	
 	public Brand findByName(String name);
+	
+	@Query("SELECT b FROM Brand b WHERE b.name LIKE %?1%")
+	public Page<Brand> findAll(String keyword, Pageable pageable);
 	
 }
