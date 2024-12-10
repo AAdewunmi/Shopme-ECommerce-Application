6ee7dd75ac0093f60687e32e9c9c163de994f3e2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-12-10 14:32:44 +0000

    Five Hundred Forty Seven Commit: Refactor BrandRepository class

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/brand/BrandRepository.java
@@ -4,11 +4,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
 import com.shopme.common.entity.Brand;
 
-
-
-public interface BrandRepository extends CrudRepository<Brand, Integer>{
+public interface BrandRepository extends CrudRepository<Brand, Integer>,
+					PagingAndSortingRepository<Brand, Integer>{
 	
 	public Long countById(Integer id);
 	
