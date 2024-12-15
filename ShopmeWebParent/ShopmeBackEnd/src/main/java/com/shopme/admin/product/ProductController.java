18e2f37ce6baed15246e60957d2b86a10602b5fc
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-12-15 12:50:33 +0000

    Five Hundred Sixty Seven Commit: Implement listAll() method in ProductController class

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -1,5 +1,28 @@
 package com.shopme.admin.product;
 
-public class ProductController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.shopme.admin.brand.BrandService;
+import com.shopme.common.entity.Product;
+
+@Controller
+@CrossOrigin
+public class ProductController {
+	@Autowired private ProductService productService;
+	@Autowired private BrandService brandService;
+	
+	@GetMapping("/products")
+	public String listAll(Model model) {
+		List<Product> listProducts = productService.listAll();
+		
+		model.addAttribute("listProducts", listProducts);
+		
+		return "products/products";
+	}
 }
