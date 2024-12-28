793b85c7e027cb5e5c373be84610a4766e13242a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-12-28 09:56:57 +0000

    Six Hundred Fifty Three Commit: Implement setProductDetails() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -160,6 +160,23 @@ public class ProductController {
 			}
 		}
 	}
+	
+	private void setProductDetails(String[] detailIDs, String[] detailNames, 
+			String[] detailValues, Product product) {
+		if (detailNames == null || detailNames.length == 0) return;
+		
+		for (int count = 0; count < detailNames.length; count++) {
+			String name = detailNames[count];
+			String value = detailValues[count];
+			Integer id = Integer.parseInt(detailIDs[count]);
+			
+			if (id != 0) {
+				product.addDetail(id, name, value);
+			} else if (!name.isEmpty() && !value.isEmpty()) {
+				product.addDetail(name, value);
+			}
+		}
+	}
 
 	@GetMapping("/products/{id}/enabled/{status}")
 	public String updateCategoryEnabledStatus(@PathVariable("id") Integer id,
