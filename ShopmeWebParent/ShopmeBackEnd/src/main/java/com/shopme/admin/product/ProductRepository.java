a368af1f5628e68551b953c42fedc9731b4a98c9
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2024-12-29 10:09:36 +0000

    Six Hundred Sixty Five Commit: Declare findAll() interface method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
@@ -1,13 +1,17 @@
 package com.shopme.admin.product;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.shopme.common.entity.Product;
 
 
-public interface ProductRepository extends CrudRepository<Product, Integer>{
+public interface ProductRepository extends CrudRepository<Product, Integer>,
+PagingAndSortingRepository<Product, Integer>{
 	
 	public Product findByName(String name);
 	
@@ -16,5 +20,12 @@ public interface ProductRepository extends CrudRepository<Product, Integer>{
 	public void updateEnabledStatus(Integer id, boolean enabled);
 	
 	public Long countById(Integer id);
+	
+	@Query("SELECT p FROM Product p WHERE p.name LIKE %?1%"
+			+ "OR p.shortDescription LIKE %?1% "
+			+ "OR p.fullDescription LIKE %?1% "
+			+ "OR p.brand.name LIKE %?1% "
+			+ "OR p.category.name LIKE %?1% ")
+	public Page<Product> findAll(String keyword, Pageable pageable);
 	
 }
