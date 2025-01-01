3303a322d31189d0d44ae2a40e62f9d64928e737
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-01 16:36:39 +0000

    Six Hundred Eight Seven Commit: Declare searchInCategory() abstract method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductRepository.java
@@ -33,4 +33,14 @@ PagingAndSortingRepository<Product, Integer>{
 	public Page<Product> findAllInCategory(Integer categoryId, String categoryIdMatch, 
 			Pageable pageable);
 	
+	@Query("SELECT p FROM Product p WHERE (p.category.id = ?1 "
+			+ "OR p.category.allParentID LIKE %?2%) AND "
+			+ "(p.name LIKE %?3% " 
+			+ "OR p.shortDescription LIKE %?3% "
+			+ "OR p.fullDescription LIKE %?3% "
+			+ "OR p.brand.name LIKE %?3% "
+			+ "OR p.category.name LIKE %?3%)")			
+	public Page<Product> searchInCategory(Integer categoryId, String categoryIdMatch, 
+			String keyword, Pageable pageable);
+	
 }
