356264bd500554706ea7b3bb5ac609ee12213fc2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-05 11:01:42 +0000

    Seven Hundred Twenty Six Commit: Implement addResourceHandlers() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MvcConfig.java
@@ -12,19 +12,10 @@ public class MvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		exposeDirectory("user-photos", registry);
 		exposeDirectory("../category-images", registry);
 		exposeDirectory("../brand-logos", registry);
 		exposeDirectory("../product-images", registry);
 	}
 	
-	private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) {
-		Path path = Paths.get(pathPattern);
-		String absolutePath = path.toFile().getAbsolutePath();
-		
-		String logicalPath = pathPattern.replace("../", "") + "/**";
-				
-		registry.addResourceHandler(logicalPath)
-			.addResourceLocations("file://" + absolutePath + "/");		
-	}
+	
 }
