7e371d18b1fc4ed1db3d85aa19925f5b1332a431
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-06 17:28:01 +0000

    Seven Hundred Thirty Five Commit: Refactor viewCategoryFirstPage() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductController.java
@@ -1,11 +1,7 @@
 package com.shopme.product;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,8 +17,11 @@ public class ProductController {
 	private CategoryService categoryService;
 	
 	@GetMapping("/c/{category_alias}")
-	public String viewCategoryFirstPage(@PathVariable("category_alias") String alias,
-			Model model) {
+	public String viewCategoryFirstPage(@PathVariable("category_alias") String alias) {
+		Category category = categoryService.getCategory(alias);
+		if (category == null) {
+			return "error/404";
+		}
 		return "products_by_category";
 	}
 	
