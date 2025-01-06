45aa82662d1667992704d75c921d89be0f0c3ee6
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-06 16:55:46 +0000

    Seven Hundred Thirty Two Commit: Implement viewCategoryFirstPage() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductController.java
@@ -1,5 +1,25 @@
 package com.shopme.product;
 
-public class ProductController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.shopme.category.CategoryService;
+
+@Controller
+@CrossOrigin
+public class ProductController {
+	
+	@Autowired 
+	private CategoryService categoryService;
+	
+	@GetMapping("/c/{category_alias}")
+	public String viewCategoryFirstPage(@PathVariable("category_alias") String alias,
+			Model model) {
+		return viewCategoryByPage(alias, 1, model);
+	}
+	
 }
