5d564bcb0e0f8f90e2304ab0a8a30934b31c811f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-06 17:02:17 +0000

    Seven Hundred Thirty Three Commit: Create skeleton products_by_category.html

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductController.java
@@ -1,6 +1,9 @@
 package com.shopme.product;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.shopme.category.CategoryService;
+import com.shopme.common.entity.Category;
 
 @Controller
 @CrossOrigin
@@ -19,7 +23,7 @@ public class ProductController {
 	@GetMapping("/c/{category_alias}")
 	public String viewCategoryFirstPage(@PathVariable("category_alias") String alias,
 			Model model) {
-		return viewCategoryByPage(alias, 1, model);
+		return "products_by_category";
 	}
 	
 }
