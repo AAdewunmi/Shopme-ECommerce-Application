9f9eab34131aa239770dbfbe10f1af06b0c0f523
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-07 16:42:15 +0000

    Seven Hundred Forty Two Commit: Implement viewCategoryByPage() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductController.java
@@ -1,16 +1,16 @@
 package com.shopme.product;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import com.shopme.category.CategoryService;
 import com.shopme.common.entity.Category;
+import com.shopme.common.entity.Product;
 
 @Controller
 @CrossOrigin
@@ -19,16 +19,35 @@ public class ProductController {
 	@Autowired 
 	private CategoryService categoryService;
 	
+	@Autowired
+	private ProductService productService;
+	
 	@GetMapping("/c/{category_alias}")
-	public String viewCategoryFirstPage(@PathVariable("category_alias") String alias,
+	public String viewCategoryByPage(@PathVariable("category_alias") String alias,
+			@PathVariable("pageNum") Integer pageNum,
 			Model model) {
 		Category category = categoryService.getCategory(alias);
 		if (category == null) {
 			return "error/404";
 		}
 		List<Category> listCategoryParents = categoryService.getCategoryParents(category);
+		Page<Product> pageProducts = productService.listByCategory(1, category.getId());
+		List<Product> listProducts = pageProducts.getContent();
+		
+		long startCount = (pageNum - 1) * ProductService.PRODUCTS_PER_PAGE + 1;
+		long endCount = startCount + ProductService.PRODUCTS_PER_PAGE - 1;
+		if (endCount > pageProducts.getTotalElements()) {
+			endCount = pageProducts.getTotalElements();
+		}
+			
+		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("totalPages", pageProducts.getTotalPages());
+		model.addAttribute("startCount", startCount);
+		model.addAttribute("endCount", endCount);
+		model.addAttribute("totalItems", pageProducts.getTotalElements());
 		model.addAttribute("pageTitle", category.getName());
 		model.addAttribute("listCategoryParents", listCategoryParents);
+		model.addAttribute("listProducts", listProducts);
 		return "products_by_category";
 	}
 	
