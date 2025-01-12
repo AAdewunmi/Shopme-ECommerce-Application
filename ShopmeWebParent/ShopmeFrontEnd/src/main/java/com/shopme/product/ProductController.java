478a15db75c8af36a77d685286d4179d6d9d8880
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-12 09:05:20 +0000

    Seven Hundred Seventy Commit: Implement search() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/product/ProductController.java
@@ -3,6 +3,7 @@ package com.shopme.product;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -78,6 +79,11 @@ public class ProductController {
 		} catch (ProductNotFoundException e) {
 			return "error/404";
 		}
+	}
+	
+	@GetMapping("/search")
+	public String search(@Param("keyword") String keyword, Model model) {
+		return "product/search_results";
 	}
 	
 }
