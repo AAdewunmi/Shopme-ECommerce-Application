bee6f1967b69f411744ecbd06a2dbc470f6dcb7c
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-13 15:10:07 +0000

    Seven Hundred Ninety Commit: Implement testCreateCurrencies() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/CurrencyRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/CurrencyRepository.java
@@ -1,9 +1,11 @@
 package com.shopme.admin.setting;
 
-import java.util.Currency;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.shopme.common.entity.Currency;
+
 public interface CurrencyRepository extends CrudRepository<Currency, Integer>{
+
 
 }
