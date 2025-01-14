ac9d7e02f2ec79f061ceefccc4e450977a0543ad
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-14 14:22:49 +0000

    Seven Hundred Ninety Nine Commit: Implement listAll() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/SettingController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/SettingController.java
@@ -1,5 +1,37 @@
 package com.shopme.admin.setting;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.shopme.common.entity.Currency;
+import com.shopme.common.entity.Setting;
+
+@Controller
+@CrossOrigin
 public class SettingController {
+	
+	@Autowired 
+	private SettingsService service;
+	
+	@Autowired private CurrencyRepository currencyRepo;
+	
+	@GetMapping("/settings")
+	public String listAll(Model model) {
+		List<Setting> listSettings = service.listAllSettings();
+		List<Currency> listCurrencies = currencyRepo.findAllByOrderByNameAsc();
+		
+		model.addAttribute("listCurrencies", listCurrencies);
+		
+		for (Setting setting : listSettings) {
+			model.addAttribute(setting.getKey(), setting.getValue());
+		}
+		
+		return "settings/settings";
+	}
 
 }
