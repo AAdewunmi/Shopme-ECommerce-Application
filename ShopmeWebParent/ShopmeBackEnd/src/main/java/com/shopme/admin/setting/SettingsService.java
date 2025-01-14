cd0415a3fe3602f25bddaaaaeea558b9d4c38597
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-14 14:11:16 +0000

    Seven Hundred Ninety Seven Commit: Implement listAllSettings() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/SettingsService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/SettingsService.java
@@ -1,5 +1,18 @@
 package com.shopme.admin.setting;
 
-public class SettingsService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shopme.common.entity.Setting;
+
+@Service
+public class SettingsService {
+	@Autowired 
+	private SettingRepository repo;
+	
+	public List<Setting> listAllSettings() {
+		return (List<Setting>) repo.findAll();
+	}
 }
