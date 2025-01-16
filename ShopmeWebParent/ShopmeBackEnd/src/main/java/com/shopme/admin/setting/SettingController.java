c1a0d89c342e9b67b7e17dd621fef267302d435a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-16 12:14:05 +0000

    Eight Hundred Twenty Two Commit: Implement saveCurrencySymbol() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/SettingController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/SettingController.java
@@ -2,10 +2,12 @@ package com.shopme.admin.setting;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.admin.FileUploadUtil;
 import com.shopme.common.entity.Currency;
 import com.shopme.common.entity.Setting;
 
@@ -65,6 +68,16 @@ public class SettingController {
 			String uploadDir = "../site-logo/";
 			FileUploadUtil.cleanDir(uploadDir);
 			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+		}
+	}
+	
+	private void saveCurrencySymbol(HttpServletRequest request, GeneralSettingBag settingBag) {
+		Integer currencyId = Integer.parseInt(request.getParameter("CURRENCY_ID"));
+		Optional<Currency> findByIdResult = currencyRepo.findById(currencyId);
+		
+		if (findByIdResult.isPresent()) {
+			Currency currency = findByIdResult.get();
+			settingBag.updateCurrencySymbol(currency.getSymbol());
 		}
 	}
 
