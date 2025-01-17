c56c5295bf8cb0e6ea7046cc01c20152a7484fa9
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-17 14:18:05 +0000

    Eight Hundred Forty Two Commit: Implement findAllByOrderByNameAsc() abstract method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/country/CountryRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/country/CountryRepository.java
@@ -1,7 +1,11 @@
 package com.shopme.admin.setting.country;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
-public interface CountryRepository extends CrudRepository<T, ID> {
+import com.shopme.common.entity.Country;
 
+public interface CountryRepository extends CrudRepository<Country, Integer> {
+	public List<Country> findAllByOrderByNameAsc();
 }
