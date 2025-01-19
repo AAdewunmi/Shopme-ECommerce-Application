5672c18f8455fc35f312faf4d8d777f712e74636
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-19 09:13:35 +0000

    Eight Hundred Fifty Two Commit: Implement testCreateStatesInIndia() method

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/setting/state/StateRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/setting/state/StateRepositoryTests.java
@@ -1,5 +1,41 @@
 package com.shopme.admin.setting.state;
 
-public class StateRepositoryTests {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Country;
+import com.shopme.common.entity.State;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class StateRepositoryTests {
+	
+	@Autowired private StateRepository repo;
+	@Autowired private TestEntityManager entityManager;
+	
+	@Test
+	public void testCreateStatesInIndia() {
+		Integer countryId = 1;
+		Country country = entityManager.find(Country.class, countryId);
+		
+//		State state = repo.save(new State("Karnataka", country));
+//		State state = repo.save(new State("Punjab", country));
+//		State state = repo.save(new State("Uttar Pradesh", country));
+		State state = repo.save(new State("West Bengal", country));
+		
+		assertThat(state).isNotNull();
+		assertThat(state.getId()).isGreaterThan(0);
+	}
+	
 }
