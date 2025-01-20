a28a38cf911a87375a31e83b8be5efdc764f6c01
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-20 13:13:22 +0000

    Eight Hundred Fifty Nine Commit: Implement listAll() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/country/CountryRestController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/country/CountryRestController.java
@@ -1,5 +1,24 @@
 package com.shopme.admin.setting.country;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shopme.common.entity.Country;
+
+@RestController
 public class CountryRestController {
+	
+@Autowired private CountryRepository repo;
+	
+	@GetMapping("/countries/list")
+	public List<Country> listAll() {
+		return repo.findAllByOrderByNameAsc();
+	}
 
 }
