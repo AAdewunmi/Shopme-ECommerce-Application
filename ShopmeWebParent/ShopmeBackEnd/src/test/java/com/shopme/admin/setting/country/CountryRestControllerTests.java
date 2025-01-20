075e4452b54c254717b44e2d20f405bf4b3c0219
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-20 14:40:29 +0000

    Eight Hundred Sixty Five Commit: Implement  testCreateCountry() method

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/setting/country/CountryRestControllerTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/setting/country/CountryRestControllerTests.java
@@ -29,11 +29,14 @@ import com.shopme.common.entity.Country;
 @SpringBootTest
 @AutoConfigureMockMvc
 public class CountryRestControllerTests {
-@Autowired MockMvc mockMvc;
+	@Autowired 
+	MockMvc mockMvc;
 	
-	@Autowired ObjectMapper objectMapper;
+	@Autowired 
+	ObjectMapper objectMapper;
 	
-	@Autowired CountryRepository repo;
+	@Autowired 
+	CountryRepository repo;
 	
 	@Test
 	@WithMockUser(username = "nam@codejava.net", password = "something", roles = "ADMIN")
@@ -47,5 +50,56 @@ public class CountryRestControllerTests {
 		String jsonResponse = result.getResponse().getContentAsString();
 		Country[] countries = objectMapper.readValue(jsonResponse, Country[].class);
 		assertThat(countries).hasSizeGreaterThan(0);
+	}
+	
+	@Test
+	@WithMockUser(username = "nam@codejava.net", password = "something", roles = "ADMIN")
+	public void testCreateCountry() throws JsonProcessingException, Exception {
+		String url = "/countries/save";
+		String countryName = "Canada";
+		String countryCode = "CA";
+		Country country = new Country(countryName, countryCode);
+		
+		MvcResult result = mockMvc.perform(post(url).contentType("application/json")
+				.content(objectMapper.writeValueAsString(country))
+				.with(csrf()))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andReturn();
+		
+		String response = result.getResponse().getContentAsString();
+		Integer countryId = Integer.parseInt(response);
+		
+		Optional<Country> findById = repo.findById(countryId);
+		assertThat(findById.isPresent());
+		
+		Country savedCountry = findById.get();
+		
+		assertThat(savedCountry.getName()).isEqualTo(countryName);
+	}
+	
+	@Test
+	@WithMockUser(username = "nam@codejava.net", password = "something", roles = "ADMIN")
+	public void testUpdateCountry() throws JsonProcessingException, Exception {
+		String url = "/countries/save";
+		
+		Integer countryId = 1;
+		String countryName = "China";
+		String countryCode = "CN";
+		Country country = new Country(countryId, countryName, countryCode);
+		
+		mockMvc.perform(post(url).contentType("application/json")
+				.content(objectMapper.writeValueAsString(country))
+				.with(csrf()))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(content().string(String.valueOf(countryId)));
+				
+		Optional<Country> findById = repo.findById(countryId);
+		assertThat(findById.isPresent());
+		
+		Country savedCountry = findById.get();
+		
+		assertThat(savedCountry.getName()).isEqualTo(countryName);
 	}
 }
