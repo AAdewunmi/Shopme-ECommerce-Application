22527cef213fec782b65be47ddb58b5eaffbea90
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-21 11:35:11 +0000

    Eight Hundred Seventy One Commit: Implement JQuery ready() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/countries_settings.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/countries_settings.js
@@ -1,3 +1,43 @@
-/**
- * 
- */
+var buttonLoad;
+var dropDownCountry;
+var buttonAddCountry;
+var buttonUpdateCountry;
+var buttonDeleteCountry;
+var labelCountryName;
+var fieldCountryName;
+var fieldCountryCode;
+
+$(document).ready(function() {
+	buttonLoad = $("#buttonLoadCountries");
+	dropDownCountry = $("#dropDownCountries");
+	buttonAddCountry = $("#buttonAddCountry");
+	buttonUpdateCountry = $("#buttonUpdateCountry");
+	buttonDeleteCountry = $("#buttonDeleteCountry");
+	labelCountryName = $("#labelCountryName");
+	fieldCountryName = $("#fieldCountryName");
+	fieldCountryCode = $("#fieldCountryCode");
+	
+	buttonLoad.click(function() {
+		loadCountries();
+	});
+	
+	dropDownCountry.on("change", function() {
+		changeFormStateToSelectedCountry();
+	});
+	
+	buttonAddCountry.click(function() {
+		if (buttonAddCountry.val() == "Add") {
+			addCountry();
+		} else {
+			changeFormStateToNew();
+		}
+	});
+	
+	buttonUpdateCountry.click(function() {
+		updateCountry();
+	});
+	
+	buttonDeleteCountry.click(function() {
+		deleteCountry();
+	});
+});
