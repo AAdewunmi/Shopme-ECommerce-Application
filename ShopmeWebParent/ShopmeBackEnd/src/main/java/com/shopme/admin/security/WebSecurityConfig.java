e6c3ab5bd3f178f5bf3848b1d0f853ca8c0e67e4
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-23 11:25:21 +0000

    Nine Hundred Five Commit: Refactor filterChain() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -38,7 +38,7 @@ public class WebSecurityConfig {
 	@Bean
 	  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 	    http.authorizeHttpRequests(auth -> auth
-	            .requestMatchers("/users/**").hasAuthority("Admin")
+	            .requestMatchers("/users/**", "/settings/**", "/countries/**", "/states/**").hasAuthority("Admin")
 	            .requestMatchers("/categories/**", "/brands/**").hasAnyAuthority("Admin", "Editor")
 	            .requestMatchers("/products/new", "/products/delete/**").hasAnyAuthority("Admin", "Editor")
 	            .requestMatchers("/products/edit/", "/products/save", 
