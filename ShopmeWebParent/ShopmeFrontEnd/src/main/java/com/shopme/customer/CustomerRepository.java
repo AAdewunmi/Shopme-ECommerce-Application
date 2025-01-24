a8858b5997fa5f1bc5cff40671143a0590cc8f67
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-24 13:49:08 +0000

    Nine Hundred Fourteen Commit: Declare enable() abstract method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerRepository.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.shopme.customer;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -12,5 +13,9 @@ public interface CustomerRepository extends CrudRepository<Customer, Integer> {
 	
 	@Query("SELECT c FROM Customer c WHERE c.verificationCode = ?1")
 	public Customer findByVerificationCode(String code);
+	
+	@Query("UPDATE Customer c SET c.enabled = true WHERE c.id = ?1")
+	@Modifying
+	public void enable(Integer id);	
 
 }
