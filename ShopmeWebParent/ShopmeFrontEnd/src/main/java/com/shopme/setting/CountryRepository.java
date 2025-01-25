5db6c833f49678e93f8950d3842f598a0ddb6888
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-25 14:00:41 +0000

    Nine Hundred Twenty Six Commit: Declare findAllByOrderByNameAsc() abstract method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/setting/CountryRepository.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/setting/CountryRepository.java
@@ -1,5 +1,11 @@
 package com.shopme.setting;
 
+import java.util.List;
+
+import com.shopme.common.entity.Country;
+
 public interface CountryRepository {
+	
+	public List<Country> findAllByOrderByNameAsc();
 
 }
