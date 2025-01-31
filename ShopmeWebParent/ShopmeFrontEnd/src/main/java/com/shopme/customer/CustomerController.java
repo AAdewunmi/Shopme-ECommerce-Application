778974554cc5f32ca3e7e09fbcfc8f9b15bbebf0
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-31 12:44:53 +0000

    Nine Hundred Fifty Nine Commit: Create skeleton register_success.html

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerController.java
@@ -4,6 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -74,6 +75,13 @@ public class CustomerController {
 		
 		System.out.println("to Address: " + toAddress);
 		System.out.println("Verify URL: " + verifyURL);
+	}
+	
+	@GetMapping("/verify")
+	public String verifyAccount(@Param("code") String code, Model model) {
+		boolean verified = customerService.verify(code);
+		
+		return "register/" + (verified ? "verify_success" : "verify_fail");
 	}
 	
 }
