8241348b01b5c68c049e9cb326b5cf02c9c63932
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-31 12:36:49 +0000

    Nine Hundred Fifty Seven Commit: Implement createCustomer() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerController.java
@@ -1,5 +1,6 @@
 package com.shopme.customer;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,25 +8,41 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.shopme.common.entity.Country;
 import com.shopme.common.entity.Customer;
+import com.shopme.setting.SettingService;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @CrossOrigin
 public class CustomerController {
 	
-@Autowired private CustomerService service;
-	
+@Autowired private CustomerService customerService;
+@Autowired private SettingService settingService;
+
 	@GetMapping("/register")
 	public String showRegisterForm(Model model) {
-		List<Country> listCountries = service.listAllCountries();
+		List<Country> listCountries = customerService.listAllCountries();
 		
 		model.addAttribute("listCountries", listCountries);
 		model.addAttribute("pageTitle", "Customer Registration");
 		model.addAttribute("customer", new Customer());
 		
 		return "register/register_form";
+	}
+	
+	@PostMapping("/create_customer")
+	public String createCustomer(Customer customer, Model model,
+			HttpServletRequest request) throws UnsupportedEncodingException, MessagingException {
+		customerService.registerCustomer(customer);
+		sendVerificationEmail(request, customer);
+		
+		model.addAttribute("pageTitle", "Registration Succeeded!");
+		
+		return "/register/register_success";
 	}
 	
 }
