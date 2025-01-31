488107b7cbc2fa8ff4d98d991fbf6a8c7c2d08fd
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-01-31 13:33:32 +0000

    Nine Hundred Sixty Six Commit: Implement getSiteURL() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/Utility.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/Utility.java
@@ -1,5 +1,13 @@
 package com.shopme;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 public class Utility {
+	
+	public static String getSiteURL(HttpServletRequest request) {
+		String siteURL = request.getRequestURL().toString();
+		
+		return siteURL.replace(request.getServletPath(), "");
+	}
 
 }
