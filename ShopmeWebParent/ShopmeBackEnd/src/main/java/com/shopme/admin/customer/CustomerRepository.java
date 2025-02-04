a7d18c33bf8e428a5ecac2050647eb5ce38b3758
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-02-04 07:19:55 +0000

    Nine Hundred Eighty Four Commit: Declare findAll() abstract method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerRepository.java
@@ -1,7 +1,18 @@
 package com.shopme.admin.customer;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-public interface CustomerRepository extends PagingAndSortingRepository<T, ID> {
+import com.shopme.admin.paging.SearchRepository;
+import com.shopme.common.entity.Customer;
 
+public interface CustomerRepository extends SearchRepository<Customer, Integer> {
+	
+	@Query("SELECT c FROM Customer c WHERE CONCAT(c.email, ' ', c.firstName, ' ', c.lastName, ' ', "
+			+ "c.addressLine1, ' ', c.addressLine2, ' ', c.city, ' ', c.state, "
+			+ "' ', c.postalCode, ' ', c.country.name) LIKE %?1%")
+	public Page<Customer> findAll(String keyword, Pageable pageable);
+	
 }
