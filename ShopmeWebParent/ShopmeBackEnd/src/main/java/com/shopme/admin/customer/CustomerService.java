88454742b4b05d8cd150f00bab1096bea823ab7a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-02-05 08:21:56 +0000

    Nine Hundred Eighty Nine Commit: Implement listByPage() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerService.java
@@ -1,5 +1,25 @@
 package com.shopme.admin.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.shopme.admin.setting.country.CountryRepository;
+
 public class CustomerService {
+	
+public static final int CUSTOMERS_PER_PAGE = 10;
+	
+	@Autowired 
+	private CustomerRepository customerRepo;
+	
+	@Autowired 
+	private CountryRepository countryRepo;	
+	
+	@Autowired 
+	private PasswordEncoder passwordEncoder;
+	
+	public void listByPage(int pageNum, PagingAndSortingHelper helper) {
+		helper.listEntities(pageNum, CUSTOMERS_PER_PAGE, customerRepo);
+	}
 
 }
