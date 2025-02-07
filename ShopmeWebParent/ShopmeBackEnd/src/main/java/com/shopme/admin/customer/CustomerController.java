881cf5c57ac31511fe6c18970c64eb1fc247f647
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-02-07 08:32:10 +0000

    One Thousand Eight Commit: Implement listFirstPage() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerController.java
@@ -1,5 +1,31 @@
 package com.shopme.admin.customer;
 
-public class CustomerController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.shopme.admin.paging.PagingAndSortingHelper;
+import com.shopme.admin.paging.PagingAndSortingParam;
+import com.shopme.common.entity.Country;
+import com.shopme.common.entity.Customer;
+import com.shopme.common.exception.CustomerNotFoundException;
+
+@Controller
+public class CustomerController {
+	
+private String defaultRedirectURL = "redirect:/customers/page/1?sortField=firstName&sortDir=asc";
+	
+	@Autowired private CustomerService service;
+	
+	@GetMapping("/customers")
+	public String listFirstPage(Model model) {
+		return defaultRedirectURL;
+	}
+	
 }
