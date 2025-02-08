edfd409e972adcfb2345778d59be7838c4511cf3
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-02-08 11:52:15 +0000

    One Thousand Nineteen Commit: Implement editCustomer() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerController.java
@@ -63,4 +63,22 @@ private String defaultRedirectURL = "redirect:/customers/page/1?sortField=firstN
 		}
 	}
 	
+	@GetMapping("/customers/edit/{id}")
+	public String editCustomer(@PathVariable("id") Integer id, Model model, RedirectAttributes ra) {
+		try {
+			Customer customer = service.get(id);
+			List<Country> countries = service.listAllCountries();
+			
+			model.addAttribute("listCountries", countries);			
+			model.addAttribute("customer", customer);
+			model.addAttribute("pageTitle", String.format("Edit Customer (ID: %d)", id));
+			
+			return "customers/customer_form";
+			
+		} catch (CustomerNotFoundException ex) {
+			ra.addFlashAttribute("message", ex.getMessage());
+			return defaultRedirectURL;
+		}
+	}
+	
 }
