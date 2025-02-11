9d91611b95ee1e3d9a9bf9f6a80059046700b124
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-02-11 09:01:02 +0000

    One Thousand Thirty Commit: Implement supportsParameter() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/paging/PagingAndSortingArgumentResolver.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/paging/PagingAndSortingArgumentResolver.java
@@ -1,5 +1,17 @@
 package com.shopme.admin.paging;
 
-public class PagingAndSortingArgumentResolver {
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
 
+public class PagingAndSortingArgumentResolver {
+	
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+		return parameter.getParameterAnnotation(PagingAndSortingParam.class) != null;
+	}
+	
+	
 }
