605e358a7a91b665ef1c23e713bdebec473ea051
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-02-11 09:03:49 +0000

    One Thousand Thirty One Commit: Implement resolveArgument() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/paging/PagingAndSortingArgumentResolver.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/paging/PagingAndSortingArgumentResolver.java
@@ -8,10 +8,28 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 public class PagingAndSortingArgumentResolver {
 	
-	@Override
+	//@Override
 	public boolean supportsParameter(MethodParameter parameter) {
 		return parameter.getParameterAnnotation(PagingAndSortingParam.class) != null;
 	}
 	
-	
+	//@Override
+	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer model,
+			NativeWebRequest request, WebDataBinderFactory binderFactory) throws Exception {
+		PagingAndSortingParam annotation = parameter.getParameterAnnotation(PagingAndSortingParam.class);
+		String sortDir = request.getParameter("sortDir");
+		String sortField = request.getParameter("sortField");
+		String keyword = request.getParameter("keyword");
+		
+		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", reverseSortDir);
+		model.addAttribute("keyword", keyword);
+		model.addAttribute("moduleURL", annotation.moduleURL());	
+		
+		
+		return new PagingAndSortingHelper(model, annotation.listName(),
+				sortField, sortDir, keyword);
+	}
 }
