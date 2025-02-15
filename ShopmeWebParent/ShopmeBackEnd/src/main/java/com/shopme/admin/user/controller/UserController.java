6273099569c23a8ec3d2aacb16b9aacfde73b8d4
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-02-15 11:16:16 +0000

    One Thousand Forty Four Commit: Refactor listFirstPage() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/controller/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/controller/UserController.java
@@ -31,40 +31,40 @@ import jakarta.servlet.http.HttpServletResponse;
 @Controller
 public class UserController {
 	
-	@Autowired
-	private UserService service;
+	/*
+	 * @Autowired private UserService service;
+	 * 
+	 * @GetMapping("/users") public String listFirstPage(Model model) { return
+	 * listByPage(1, model, "firstName", "asc", null); }
+	 * 
+	 * @GetMapping("/users/page/{pageNum}") public String
+	 * listByPage(@PathVariable(name = "pageNum") int pageNum, Model model,
+	 * 
+	 * @Param("sortField") String sortField, @Param("sortDir") String sortDir,
+	 * 
+	 * @Param("keyword") String keyword ) { System.out.println("Sort Field: " +
+	 * sortField); System.out.println("Sort Order: " + sortDir); Page<User> page =
+	 * service.listByPage(pageNum, sortField, sortDir, keyword); List<User>
+	 * listUsers = page.getContent(); long startCount = (pageNum - 1) *
+	 * UserService.USERS_PER_PAGE + 1; long endCount = startCount +
+	 * UserService.USERS_PER_PAGE - 1; if (endCount > page.getTotalElements()) {
+	 * endCount = page.getTotalElements(); } String reverseSortDir =
+	 * sortDir.equals("asc") ? "desc" : "asc"; model.addAttribute("currentPage",
+	 * pageNum); model.addAttribute("totalPages", page.getTotalPages());
+	 * model.addAttribute("startCount", startCount); model.addAttribute("endCount",
+	 * endCount); model.addAttribute("totalItems", page.getTotalElements());
+	 * model.addAttribute("listUsers", listUsers); model.addAttribute("sortField",
+	 * sortField); model.addAttribute("sortDir", sortDir);
+	 * model.addAttribute("reverseSortDir", reverseSortDir);
+	 * model.addAttribute("keyword", keyword); return "users/users"; }
+	 */
+	
+	private String defaultRedirectURL = "redirect:/users/page/1?sortField=firstName&sortDir=asc";
+	@Autowired private UserService service;
 	
 	@GetMapping("/users")
-	public String listFirstPage(Model model) {
-		return listByPage(1, model, "firstName", "asc", null);
-	}
-	
-	@GetMapping("/users/page/{pageNum}")
-	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model,
-			@Param("sortField") String sortField, @Param("sortDir") String sortDir,
-			@Param("keyword") String keyword
-			) {
-		System.out.println("Sort Field: " + sortField);
-		System.out.println("Sort Order: " + sortDir);
-		Page<User> page = service.listByPage(pageNum, sortField, sortDir, keyword);
-		List<User> listUsers = page.getContent();
-		long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
-		long endCount = startCount + UserService.USERS_PER_PAGE - 1;
-		if (endCount > page.getTotalElements()) {
-			endCount = page.getTotalElements();
-		}
-		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
-		model.addAttribute("currentPage", pageNum);
-		model.addAttribute("totalPages", page.getTotalPages());
-		model.addAttribute("startCount", startCount);
-		model.addAttribute("endCount", endCount);
-		model.addAttribute("totalItems",  page.getTotalElements());
-		model.addAttribute("listUsers", listUsers);
-		model.addAttribute("sortField", sortField);
-		model.addAttribute("sortDir", sortDir);
-		model.addAttribute("reverseSortDir", reverseSortDir);
-		model.addAttribute("keyword", keyword);
-		return "users/users";		
+	public String listFirstPage() {
+		return defaultRedirectURL;
 	}
 	
 	@GetMapping("/users/new")
