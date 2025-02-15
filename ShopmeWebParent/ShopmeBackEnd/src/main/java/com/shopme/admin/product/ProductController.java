cd4dfc531d021ee9894b5fad5b88385b5161d877
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-02-15 13:36:52 +0000

    One Thousand Fifty Two Commit: Implement ProductController\listByPage() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -35,6 +35,8 @@ import com.shopme.common.entity.Category;
 import com.shopme.common.entity.Product;
 import com.shopme.common.entity.ProductImage;
 import com.shopme.common.exception.ProductNotFoundException;
+import com.shopme.admin.paging.PagingAndSortingHelper;
+import com.shopme.admin.paging.PagingAndSortingParam;
 
 @Controller
 @CrossOrigin
@@ -96,6 +98,23 @@ public class ProductController {
 	@GetMapping("/products")
 	public String listFirstPage(Model model) {
 		return defaultRedirectURL;
+	}
+	
+	@GetMapping("/products/page/{pageNum}")
+	public String listByPage(
+			@PagingAndSortingParam(listName = "listProducts", moduleURL = "/products") PagingAndSortingHelper helper,
+			@PathVariable(name = "pageNum") int pageNum, Model model,
+			@Param("categoryId") Integer categoryId
+			) {
+		
+		productService.listByPage(pageNum, helper, categoryId);
+		
+		List<Category> listCategories = categoryService.listCategoriesUsedInForm();
+		
+		if (categoryId != null) model.addAttribute("categoryId", categoryId);
+		model.addAttribute("listCategories", listCategories);
+		
+		return "products/products";		
 	}
 	
 	@GetMapping("/products/new")
