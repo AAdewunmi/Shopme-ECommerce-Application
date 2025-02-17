a3ca9548cb539942e3afaf76ca4d2136bcb5a302
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-02-17 08:33:52 +0000

    One Thousand Fifty Seven Commit: Implement getAuthorities() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/security/CustomerUserDetails.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/security/CustomerUserDetails.java
@@ -1,5 +1,22 @@
 package com.shopme.security;
 
-public class CustomerUserDetails {
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import com.shopme.common.entity.Customer;
+
+public class CustomerUserDetails implements UserDetails{
+	
+private Customer customer;
+	
+	public CustomerUserDetails(Customer customer) {
+		this.customer = customer;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
 
 }
