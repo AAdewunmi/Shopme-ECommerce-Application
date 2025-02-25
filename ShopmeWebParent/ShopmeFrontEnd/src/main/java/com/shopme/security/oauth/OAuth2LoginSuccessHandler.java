31fbd250fc414826d757e959b516e38ce7d87b09
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-02-25 11:37:20 +0000

    One Thousand Ninety Eight Commit: Implement onAuthenticationSuccess() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/security/oauth/OAuth2LoginSuccessHandler.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/security/oauth/OAuth2LoginSuccessHandler.java
@@ -1,5 +1,45 @@
 package com.shopme.security.oauth;
 
-public class OAuth2LoginSuccessHandler {
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+import com.shopme.common.entity.AuthenticationType;
+import com.shopme.common.entity.Customer;
+import com.shopme.customer.CustomerService;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class OAuth2LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler{
+	
+	@Autowired 
+	private CustomerService customerService;
+	
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws ServletException, IOException {
+		CustomerOAuth2User oauth2User = (CustomerOAuth2User) authentication.getPrincipal();
+		
+		String name = oauth2User.getName();
+		String email = oauth2User.getEmail();
+		String countryCode = request.getLocale().getCountry();
+		String clientName = oauth2User.getClientName();
+		
+		AuthenticationType authenticationType = getAuthenticationType(clientName);
+		
+		Customer customer = customerService.getCustomerByEmail(email);
+		if (customer == null) {
+			customerService.addNewCustomerUponOAuthLogin(name, email, countryCode, authenticationType);
+		} else {
+			oauth2User.setFullName(customer.getFullName());
+			customerService.updateAuthenticationType(customer, authenticationType);
+		}
+		
+		super.onAuthenticationSuccess(request, response, authentication);
+	}
 
 }
