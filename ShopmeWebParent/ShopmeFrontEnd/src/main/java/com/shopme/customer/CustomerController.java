c8ea58ccc230d5178eadfd2c12ab7507bdb9cc7a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-02-27 09:37:36 +0000

    One Thousand One Hundred Twelve Commit: Implement getEmailOfAuthenticatedCustomer() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerController.java
@@ -101,4 +101,20 @@ public class CustomerController {
 		return "customer/account_form";
 	}
 	
+	private String getEmailOfAuthenticatedCustomer(HttpServletRequest request) {
+		Object principal = request.getUserPrincipal();
+		String customerEmail = null;
+		
+		if (principal instanceof UsernamePasswordAuthenticationToken 
+				|| principal instanceof RememberMeAuthenticationToken) {
+			customerEmail = request.getUserPrincipal().getName();
+		} else if (principal instanceof OAuth2AuthenticationToken) {
+			OAuth2AuthenticationToken oauth2Token = (OAuth2AuthenticationToken) principal;
+			CustomerOAuth2User oauth2User = (CustomerOAuth2User) oauth2Token.getPrincipal();
+			customerEmail = oauth2User.getEmail();
+		}
+		
+		return customerEmail;
+	}
+	
 }
