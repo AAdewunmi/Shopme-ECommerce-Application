ed2d149b0fb90268091023f73708037ca537239e
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-02 14:05:59 +0000

    One Thousand One Hundred Thirty Commit: Implement showRegisterForm() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerController.java
@@ -15,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.Utility;
 import com.shopme.common.entity.Country;
