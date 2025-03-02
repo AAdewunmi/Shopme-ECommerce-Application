49c34d780eb3fb85b226e3ade0f18c0a1fbf41db
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-02 14:08:26 +0000

    One Thousand One Hundred Thirty Two Commit: Refactor save() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/customer/CustomerService.java
@@ -77,6 +77,7 @@ public static final int CUSTOMERS_PER_PAGE = 10;
 		customerInForm.setCreatedTime(customerInDB.getCreatedTime());
 		customerInForm.setVerificationCode(customerInDB.getVerificationCode());
 		customerInForm.setResetPasswordToken(customerInDB.getResetPasswordToken());
+		customerInForm.setResetPasswordToken(customerInDB.getResetPasswordToken());
 		customerRepo.save(customerInForm);
 	}
 	
