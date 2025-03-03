450b58db256be4639cacedf5afb8f1379026a796
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-03 15:32:01 +0000

    One Thousand One Hundred Thirty Four Commit: ImplementString showRequestForm() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/ForgotPasswordController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/ForgotPasswordController.java
@@ -1,5 +1,21 @@
 package com.shopme.customer;
 
-public class ForgotPasswordController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.shopme.setting.SettingService;
+
+@Controller
+public class ForgotPasswordController {
+	@Autowired 
+	private CustomerService customerService;
+	
+	@Autowired 
+	private SettingService settingService;
+	
+	@GetMapping("/forgot_password")
+	public String showRequestForm() {
+		return "customer/forgot_password_form";
+	}
 }
