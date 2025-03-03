0742c725074b2623fca7ed4bfb49c24c51a1fa80
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-03 15:41:37 +0000

    One Thousand One Hundred Thirty Five Commit: Implement processRequestForm() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/ForgotPasswordController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/ForgotPasswordController.java
@@ -1,10 +1,19 @@
 package com.shopme.customer;
 
+import java.io.UnsupportedEncodingException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.shopme.Utility;
+import com.shopme.common.exception.CustomerNotFoundException;
 import com.shopme.setting.SettingService;
+
+import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class ForgotPasswordController {
@@ -16,6 +25,25 @@ public class ForgotPasswordController {
 	
 	@GetMapping("/forgot_password")
 	public String showRequestForm() {
+		return "customer/forgot_password_form";
+	}
+	
+	@PostMapping("/forgot_password")
+	public String processRequestForm(HttpServletRequest request, Model model) {
+		String email = request.getParameter("email");
+		try {
+			String token = customerService.updateResetPasswordToken(email);
+			String link = Utility.getSiteURL(request) + "/reset_password?token=" + token;
+			sendEmail(link, email);
+			
+			model.addAttribute("message", "We have sent a reset password link to your email."
+					+ " Please check.");
+		} catch (CustomerNotFoundException e) {
+			model.addAttribute("error", e.getMessage());
+		} catch (UnsupportedEncodingException | MessagingException e) {
+			model.addAttribute("error", "Could not send email");
+		}
+		
 		return "customer/forgot_password_form";
 	}
 }
