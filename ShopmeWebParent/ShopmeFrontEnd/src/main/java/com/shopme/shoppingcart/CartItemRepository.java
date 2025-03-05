2e468958205967c841a20873e9de5d16b254190b
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-05 14:18:36 +0000

    One Thousand One Hundred Fifty Six Commit: Declare findByCustomer() abstract method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/CartItemRepository.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/CartItemRepository.java
@@ -1,7 +1,17 @@
 package com.shopme.shoppingcart;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CartItemRepository extends CrudRepository<T, ID> {
+import com.shopme.common.entity.CartItem;
+import com.shopme.common.entity.Customer;
+import com.shopme.common.entity.Product;
+
+public interface CartItemRepository extends CrudRepository<CartItem, Integer> {
+	
+	public List<CartItem> findByCustomer(Customer customer);
 
 }
