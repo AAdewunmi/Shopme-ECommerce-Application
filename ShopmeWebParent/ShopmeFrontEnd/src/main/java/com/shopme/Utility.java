4318af5c97598204c02363eb537a72e16fbdfd60
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-07 12:38:41 +0000

    One Thousand One Hundred Eighty Six Commit: Implement getEmailOfAuthenticatedCustomer() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/Utility.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/Utility.java
@@ -3,7 +3,11 @@ package com.shopme;
 import java.util.Properties;
 
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.security.authentication.RememberMeAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 
+import com.shopme.security.oauth.CustomerOAuth2User;
 import com.shopme.setting.EmailSettingBag;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,7 +16,6 @@ public class Utility {
 	
 	public static String getSiteURL(HttpServletRequest request) {
 		String siteURL = request.getRequestURL().toString();
-		
 		return siteURL.replace(request.getServletPath(), "");
 	}
 	
@@ -32,4 +35,22 @@ public class Utility {
 		
 		return mailSender;
 	}
+	
+	public static String getEmailOfAuthenticatedCustomer(HttpServletRequest request) {
+		Object principal = request.getUserPrincipal();
+		if (principal == null) return null;
+		
+		String customerEmail = null;
+		
+		if (principal instanceof UsernamePasswordAuthenticationToken 
+				|| principal instanceof RememberMeAuthenticationToken) {
+			customerEmail = request.getUserPrincipal().getName();
+		} else if (principal instanceof OAuth2AuthenticationToken) {
+			OAuth2AuthenticationToken oauth2Token = (OAuth2AuthenticationToken) principal;
+			CustomerOAuth2User oauth2User = (CustomerOAuth2User) oauth2Token.getPrincipal();
+			customerEmail = oauth2User.getEmail();
+		}
+		
+		return customerEmail;
+	}	
 }
