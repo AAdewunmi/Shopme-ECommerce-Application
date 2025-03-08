0513eff1b28a941a2e9f18fd47c755c662e6080b
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-08 10:09:17 +0000

    One Thousand One Hundred Ninety One Commit: Implement viewCart() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartController.java
@@ -1,5 +1,44 @@
 package com.shopme.shoppingcart;
 
-public class ShoppingCartController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.shopme.Utility;
+import com.shopme.common.entity.CartItem;
+import com.shopme.common.entity.Customer;
+import com.shopme.common.exception.CustomerNotFoundException;
+import com.shopme.customer.CustomerService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+@CrossOrigin
+public class ShoppingCartController {
+	@Autowired 
+	private CustomerService customerService;
+	
+	@Autowired 
+	private ShoppingCartService cartService;
+	
+	@GetMapping("/cart")
+	public String viewCart(Model model, HttpServletRequest request) {
+		Customer customer = getAuthenticatedCustomer(request);
+		List<CartItem> cartItems = cartService.listCartItems(customer);
+		
+		float estimatedTotal = 0.0F;
+		
+		for (CartItem item : cartItems) {
+			estimatedTotal += item.getSubtotal();
+		}
+		
+		model.addAttribute("cartItems", cartItems);
+		model.addAttribute("estimatedTotal", estimatedTotal);
+		
+		return "cart/shopping_cart";
+	}
 }
