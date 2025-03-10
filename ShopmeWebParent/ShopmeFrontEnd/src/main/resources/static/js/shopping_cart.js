576a8b2fd720d271f611b39089d37f1ece3971bf
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-10 14:06:42 +0000

    One Thousand Two Hundred One Commit: Implement JS ready() function

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/shopping_cart.js
@@ -0,0 +1,19 @@
+decimalSeparator = decimalPointType == 'COMMA' ? ',' : '.';
+thousandsSeparator = thousandsPointType == 'COMMA' ? ',' : '.'; 
+
+$(document).ready(function() {
+	$(".linkMinus").on("click", function(evt) {
+		evt.preventDefault();
+		decreaseQuantity($(this));
+	});
+	
+	$(".linkPlus").on("click", function(evt) {
+		evt.preventDefault();
+		increaseQuantity($(this));
+	});
+	
+	$(".linkRemove").on("click", function(evt) {
+		evt.preventDefault();
+		removeProduct($(this));
+	});		
+});
