00ecbdd43b6efb93ed2c8323bc90ad0da6ee5c0f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-10 13:38:11 +0000

    One Thousand One hundred Ninety Six Commit: Implement updateQuantity() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shoppingcart/ShoppingCartService.java
@@ -8,12 +8,16 @@ import org.springframework.stereotype.Service;
 import com.shopme.common.entity.CartItem;
 import com.shopme.common.entity.Customer;
 import com.shopme.common.entity.Product;
+import com.shopme.product.ProductRepository;
 
 @Service
 public class ShoppingCartService {
 	
 	@Autowired 
 	private CartItemRepository cartRepo;
+	
+	@Autowired 
+	private ProductRepository productRepo;
 	
 	public Integer addProduct(Integer productId, Integer quantity, Customer customer) 
 			throws ShoppingCartException {
@@ -45,5 +49,12 @@ public class ShoppingCartService {
 	
 	public List<CartItem> listCartItems(Customer customer) {
 		return cartRepo.findByCustomer(customer);
+	}
+	
+	public float updateQuantity(Integer productId, Integer quantity, Customer customer) {
+		cartRepo.updateQuantity(quantity, customer.getId(), productId);
+		Product product = productRepo.findById(productId).get();
+		float subtotal = product.getDiscountPrice() * quantity;
+		return subtotal;
 	}
 }
