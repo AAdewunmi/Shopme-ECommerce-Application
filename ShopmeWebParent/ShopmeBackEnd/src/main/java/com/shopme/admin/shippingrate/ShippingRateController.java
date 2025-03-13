d499d2426fcb270bcafa3ff71bedb62f1b36261a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-13 13:01:45 +0000

    One Thousand Two Hundred Twenty Five Commit: Implement listFirstPage() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateController.java
@@ -1,5 +1,28 @@
 package com.shopme.admin.shippingrate;
 
-public class ShippingRateController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.shopme.admin.paging.PagingAndSortingHelper;
+import com.shopme.admin.paging.PagingAndSortingParam;
+import com.shopme.common.entity.Country;
+import com.shopme.common.entity.ShippingRate;
+
+@Controller
+public class ShippingRateController {
+private String defaultRedirectURL = "redirect:/shipping_rates/page/1?sortField=country&sortDir=asc";
+	
+	@Autowired private ShippingRateService service;
+	
+	@GetMapping("/shipping_rates")
+	public String listFirstPage() {
+		return defaultRedirectURL;
+	}
 }
