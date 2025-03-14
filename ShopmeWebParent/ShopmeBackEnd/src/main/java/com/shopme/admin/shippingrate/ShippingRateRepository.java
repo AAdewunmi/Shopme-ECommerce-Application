659a8d73f8a07760ecad8e6147f41ad0b23aa920
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-14 12:21:46 +0000

    One Thousand Two Hundred Thirty Seven Commit: Declare findAll() abstract method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateRepository.java
@@ -1,5 +1,7 @@
 package com.shopme.admin.shippingrate;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
@@ -14,5 +16,8 @@ public interface ShippingRateRepository extends SearchRepository<ShippingRate, I
 	@Query("UPDATE ShippingRate sr SET sr.codSupported = ?2 WHERE sr.id = ?1")
 	@Modifying
 	public void updateCODSupport(Integer id, boolean enabled);
+	
+	@Query("SELECT sr FROM ShippingRate sr WHERE sr.country.name LIKE %?1% OR sr.state LIKE %?1%")
+	public Page<ShippingRate> findAll(String keyword, Pageable pageable);
 
 }
