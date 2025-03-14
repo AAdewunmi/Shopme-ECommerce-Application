f5751cf0a7e6ae2bce4da141ad434a0ded363996
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-14 12:03:11 +0000

    One Thousand Two Hundred Thirty Four Commit: Create skeleton ShippingRateRepository class

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateRepository.java
@@ -0,0 +1,8 @@
+package com.shopme.admin.shippingrate;
+
+import com.shopme.admin.paging.SearchRepository;
+import com.shopme.common.entity.ShippingRate;
+
+public interface ShippingRateRepository extends SearchRepository<ShippingRate, Integer> {
+
+}
