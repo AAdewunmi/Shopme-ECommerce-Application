c291b0f9f8e9a1c758b6b0a5de5abfcc61b7f737
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-14 11:32:40 +0000

    One Thousand Two Hundred Thirty Three Commit: Implement ShippingRateNotFoundException user defined exception class

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateNotFoundException.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateNotFoundException.java
@@ -1,5 +1,7 @@
 package com.shopme.admin.shippingrate;
 
-public class ShippingRateNotFoundException {
-
+public class ShippingRateNotFoundException extends Exception {
+	public ShippingRateNotFoundException(String message) {
+		super(message);
+	}
 }
