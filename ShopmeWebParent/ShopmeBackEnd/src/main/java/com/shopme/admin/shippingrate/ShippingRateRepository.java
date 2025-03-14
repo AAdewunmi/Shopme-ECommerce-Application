038ab37b407940312b4d4bdbb136abd8920e2ed2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-14 12:19:27 +0000

    One Thousand Two Hundred Thirty Six Commit: Declare updateCODSupport() abstract method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateRepository.java
@@ -1,5 +1,6 @@
 package com.shopme.admin.shippingrate;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.shopme.admin.paging.SearchRepository;
@@ -9,5 +10,9 @@ public interface ShippingRateRepository extends SearchRepository<ShippingRate, I
 	
 	@Query("SELECT sr FROM ShippingRate sr WHERE sr.country.id = ?1 AND sr.state = ?2")
 	public ShippingRate findByCountryAndState(Integer countryId, String state);
+	
+	@Query("UPDATE ShippingRate sr SET sr.codSupported = ?2 WHERE sr.id = ?1")
+	@Modifying
+	public void updateCODSupport(Integer id, boolean enabled);
 
 }
