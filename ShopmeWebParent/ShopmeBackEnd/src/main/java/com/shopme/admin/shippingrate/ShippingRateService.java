1e7f437e2ce4042dfe9f256d33cb7a007ddc6bcd
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-15 08:43:53 +0000

    One Thousand Two Hundred Forty Commit: Implement listByPage() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateService.java
@@ -1,5 +1,32 @@
 package com.shopme.admin.shippingrate;
 
-public class ShippingRateService {
+import java.util.List;
+import java.util.NoSuchElementException;
 
+import jakarta.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shopme.admin.paging.PagingAndSortingHelper;
+import com.shopme.admin.setting.country.CountryRepository;
+import com.shopme.common.entity.Country;
+import com.shopme.common.entity.ShippingRate;
+
+
+@Service
+@Transactional
+public class ShippingRateService {
+	public static final int RATES_PER_PAGE = 10;
+	
+	@Autowired 
+	private ShippingRateRepository shipRepo;
+	
+	@Autowired 
+	private CountryRepository countryRepo;
+	
+	public void listByPage(int pageNum, PagingAndSortingHelper helper) {
+		helper.listEntities(pageNum, RATES_PER_PAGE, shipRepo);
+	}
+	
 }
