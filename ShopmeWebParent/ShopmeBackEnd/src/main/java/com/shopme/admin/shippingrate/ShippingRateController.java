b736a1faaaf323086b2f6cc95855aec7ae6655a9
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-15 08:39:28 +0000

    One Thousand Two Hundred Thirty Nine Commit: Create skeleton ShippingRateService class

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/shippingrate/ShippingRateController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import com.shopme.common.entity.Country;
 import com.shopme.common.entity.ShippingRate;
 
 @Controller
+@CrossOrigin
 public class ShippingRateController {
 private String defaultRedirectURL = "redirect:/shipping_rates/page/1?sortField=country&sortDir=asc";
 	
