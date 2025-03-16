a7296a660cd43d58b77100a997d104e8d698d718
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-16 12:49:34 +0000

    One Thousand Two Hundred Sixty Commit: Implement JS ready() function

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form_country_state.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form_country_state.js
@@ -1,3 +1,14 @@
-/**
- * 
- */
+var dropdownCountries;
+var dropdownStates;
+
+$(document).ready(function() {
+	dropdownCountries = $("#country");
+	dropdownStates = $("#listStates");
+
+	dropdownCountries.on("change", function() {
+		loadStates4Country();
+		$("#state").val("").focus();
+	});	
+	
+	loadStates4Country();
+});
