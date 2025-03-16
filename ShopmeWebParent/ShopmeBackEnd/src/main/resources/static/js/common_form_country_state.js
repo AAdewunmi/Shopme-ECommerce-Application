1b252066ef212122e534931204964f2aaa8ed12a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-16 12:51:15 +0000

    One Thousand Two Hundred Sixty One Commit: Implement loadStates4Country() JS function

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form_country_state.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form_country_state.js
@@ -12,3 +12,20 @@ $(document).ready(function() {
 	
 	loadStates4Country();
 });
+
+function loadStates4Country() {
+	selectedCountry = $("#country option:selected");
+	countryId = selectedCountry.val();
+	
+	url = contextPath + "states/list_by_country/" + countryId;
+	
+	$.get(url, function(responseJson) {
+		dropdownStates.empty();
+		
+		$.each(responseJson, function(index, state) {
+			$("<option>").val(state.name).text(state.name).appendTo(dropdownStates);
+		});
+	}).fail(function() {
+		showErrorModal("Error loading states/provinces for the selected country.");
+	})	
+}	
