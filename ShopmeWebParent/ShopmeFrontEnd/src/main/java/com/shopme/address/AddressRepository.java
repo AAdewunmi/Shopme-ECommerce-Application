a6d627cdd42dd5d9ae27880a03d1e8f07e4beba2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-17 16:02:41 +0000

    One Thousand Two Hundred Seventy Commit: Declare findByCustomer() abstract method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/address/AddressRepository.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/address/AddressRepository.java
@@ -1,7 +1,14 @@
 package com.shopme.address;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
-public interface AddressRepository extends CrudRepository<T, ID> {
+import com.shopme.common.entity.Address;
+import com.shopme.common.entity.Customer;
+
+public interface AddressRepository extends CrudRepository<Address, Integer> {
+	
+	public List<Address> findByCustomer(Customer customer);
 
 }
