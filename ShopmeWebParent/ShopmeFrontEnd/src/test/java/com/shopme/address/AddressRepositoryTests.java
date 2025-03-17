8df4046adcde8883329e9cd1059ac0a62b5c8e66
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-17 16:38:58 +0000

    One Thousand Two Hundred Seventy Five Commit: Implement testAddNew() method

--- ShopmeWebParent/ShopmeFrontEnd/src/test/java/com/shopme/address/AddressRepositoryTests.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/test/java/com/shopme/address/AddressRepositoryTests.java
@@ -1,5 +1,48 @@
 package com.shopme.address;
 
-public class AddressRepositoryTests {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Address;
+import com.shopme.common.entity.Country;
+import com.shopme.common.entity.Customer;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class AddressRepositoryTests {
+	
+	@Autowired 
+	private AddressRepository repo;
+	
+	@Test
+	public void testAddNew() {
+		Integer customerId = 5;
+		Integer countryId = 234; // USA
+		
+		Address newAddress = new Address();
+		newAddress.setCustomer(new Customer(customerId));
+		newAddress.setCountry(new Country(countryId));
+		newAddress.setFirstName("Tobie");
+		newAddress.setLastName("Abel");
+		newAddress.setPhoneNumber("19094644165");
+		newAddress.setAddressLine1("4213 Gordon Street");
+		newAddress.setAddressLine2("Novak Building");
+		newAddress.setCity("Chino");
+		newAddress.setState("California");
+		newAddress.setPostalCode("91710");
+		
+		Address savedAddress = repo.save(newAddress);
+		
+		assertThat(savedAddress).isNotNull();
+		assertThat(savedAddress.getId()).isGreaterThan(0);
+	}
 }
