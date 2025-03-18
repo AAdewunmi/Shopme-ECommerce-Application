1b9bc50a05ab922bf8881791acceee34df81b943
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-18 14:29:03 +0000

    One Thousand Two Hundred Ninety One Commit: Implement listAddressBook() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/address/AddressService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/address/AddressService.java
@@ -1,5 +1,24 @@
 package com.shopme.address;
 
-public class AddressService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shopme.common.entity.Address;
+import com.shopme.common.entity.Customer;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class AddressService {
+	
+	@Autowired 
+	private AddressRepository repo;
+	
+	public List<Address> listAddressBook(Customer customer) {
+		return repo.findByCustomer(customer);
+	}
+	
 }
