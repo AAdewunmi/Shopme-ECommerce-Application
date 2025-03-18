4db9db3af4bf8e5e5e084fe808161aa90e9dc37e
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-18 14:11:37 +0000

    One Thousand Two Eighty Three Commit: Implement showAddressBook() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/address/AddressController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/address/AddressController.java
@@ -1,5 +1,52 @@
 package com.shopme.address;
 
-public class AddressController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.shopme.Utility;
+import com.shopme.common.entity.Address;
+import com.shopme.common.entity.Country;
+import com.shopme.common.entity.Customer;
+import com.shopme.customer.CustomerService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+@CrossOrigin
+public class AddressController {
+	
+	@Autowired 
+	private AddressService addressService;
+	
+	@Autowired 
+	private CustomerService customerService;	
+	
+	@GetMapping("/address_book")
+	public String showAddressBook(Model model, HttpServletRequest request) {
+		Customer customer = getAuthenticatedCustomer(request);
+		List<Address> listAddresses = addressService.listAddressBook(customer);
+		
+		boolean usePrimaryAddressAsDefault = true;
+		for (Address address : listAddresses) {
+			if (address.isDefaultForShipping()) {
+				usePrimaryAddressAsDefault = false;
+				break;
+			}
+		}
+		
+		model.addAttribute("listAddresses", listAddresses);
+		model.addAttribute("customer", customer);
+		model.addAttribute("usePrimaryAddressAsDefault", usePrimaryAddressAsDefault);
+		
+		return "address_book/addresses";
+	}
+	
 }
