0398f174d00c8970f9ee3664935e42a9ed0dde44
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-20 11:38:24 +0000

    One Thousand Three Hundred Three Commit: Implement getId() method

--- ShopmeCommon/src/main/java/com/shopme/common/entity/IdBasedEntity.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/IdBasedEntity.java
@@ -1,5 +1,19 @@
 package com.shopme.common.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class IdBasedEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected Integer id;
+	
+	public Integer getId() {
+		return id;
+	}
 
 }
