7b7e74403e3e9df0c9861850659b6b49b9355e1f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-23 07:58:51 +0000

    One Thousand Three Hundred Twenty Three Commit: Create skeleton OrderRepository class

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.shopme.admin.order;
+
+import com.shopme.admin.paging.SearchRepository;
+import com.shopme.common.entity.order.Order;
+
+public interface OrderRepository extends SearchRepository<Order, Integer> {
+
+}
