2917901587e90637987d4ab7565c89f51232c9c5
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-25 13:21:27 +0000

    One Thousand Three Hundred Sixty One Commit: Implement getShippingRateForAddress() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shipping/ShippingRateService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shipping/ShippingRateService.java
@@ -22,4 +22,13 @@ public class ShippingRateService {
 		return repo.findByCountryAndState(customer.getCountry(), state);
 	}
 	
+	public ShippingRate getShippingRateForAddress(Address address) {
+		String state = address.getState();
+		if (state == null || state.isEmpty()) {
+			state = address.getCity();
+		}
+		
+		return repo.findByCountryAndState(address.getCountry(), state);
+	}	
+	
 }
