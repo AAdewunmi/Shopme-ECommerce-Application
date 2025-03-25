3b35455a889e3fc5c39c3227e3589c97eb448308
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-25 13:17:41 +0000

    One Thousand Three Hundred Fifty Nine Commit: Declare findByCountryAndState() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shipping/ShippingRateRepository.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/shipping/ShippingRateRepository.java
@@ -2,6 +2,11 @@ package com.shopme.shipping;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface ShippingRateRepository extends CrudRepository<T, ID> {
+import com.shopme.common.entity.Country;
+import com.shopme.common.entity.ShippingRate;
+
+public interface ShippingRateRepository extends CrudRepository<ShippingRate, Integer> {
+	
+	public ShippingRate findByCountryAndState(Country country, String state);
 
 }
