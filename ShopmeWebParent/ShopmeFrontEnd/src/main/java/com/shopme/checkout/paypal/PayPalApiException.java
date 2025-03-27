86e7d23fd1477c00ea7e927471b5ec3eaf5ab448
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-27 12:57:45 +0000

    One Thousand Three Hundred Seventy Eight Commit: Implement user defined exception class

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/paypal/PayPalApiException.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/paypal/PayPalApiException.java
@@ -1,5 +1,9 @@
 package com.shopme.checkout.paypal;
 
-public class PayPalApiException {
-
+public class PayPalApiException extends Exception{
+	
+	public PayPalApiException(String message) {
+		super(message);
+	}
+	
 }
