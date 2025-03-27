3b2c2aa23ba0b24fa278fdcd67e4c133f90b99f0
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-27 13:32:17 +0000

    One Thousand Three Hundred Eighty Seven Commit: Create skeleton OrderService class

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/order/OrderRepository.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/order/OrderRepository.java
@@ -1,5 +1,9 @@
 package com.shopme.order;
 
-public interface OrderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.shopme.common.entity.order.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Integer>{
 
 }
