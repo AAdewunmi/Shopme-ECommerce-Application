84e140046e19ecac35ccd5fe6378d44644bbea77
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-27 14:34:04 +0000

    One Thousand Three Hundred Ninety Eight Commit: Implement processPayPalOrder() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/CheckoutController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/CheckoutController.java
@@ -91,4 +91,99 @@ public class CheckoutController {
 		return "checkout/checkout";
 	}
 	
+	private Customer getAuthenticatedCustomer(HttpServletRequest request) {
+		String email = Utility.getEmailOfAuthenticatedCustomer(request);				
+		return customerService.getCustomerByEmail(email);
+	}	
+	
+	@PostMapping("/place_order")
+	public String placeOrder(HttpServletRequest request) 
+			throws UnsupportedEncodingException, MessagingException {
+		String paymentType = request.getParameter("paymentMethod");
+		PaymentMethod paymentMethod = PaymentMethod.valueOf(paymentType);
+		
+		Customer customer = getAuthenticatedCustomer(request);
+		
+		Address defaultAddress = addressService.getDefaultAddress(customer);
+		ShippingRate shippingRate = null;
+		
+		if (defaultAddress != null) {
+			shippingRate = shipService.getShippingRateForAddress(defaultAddress);
+		} else {
+			shippingRate = shipService.getShippingRateForCustomer(customer);
+		}
+				
+		List<CartItem> cartItems = cartService.listCartItems(customer);
+		CheckoutInfo checkoutInfo = checkoutService.prepareCheckout(cartItems, shippingRate);
+		
+		Order createdOrder = orderService.createOrder(customer, defaultAddress, cartItems, paymentMethod, checkoutInfo);
+		cartService.deleteByCustomer(customer);
+		sendOrderConfirmationEmail(request, createdOrder);
+		
+		return "checkout/order_completed";
+	}
+
+	private void sendOrderConfirmationEmail(HttpServletRequest request, Order order) 
+			throws UnsupportedEncodingException, MessagingException {
+		EmailSettingBag emailSettings = settingService.getEmailSettings();
+		JavaMailSenderImpl mailSender = Utility.prepareMailSender(emailSettings);
+		mailSender.setDefaultEncoding("utf-8");
+		
+		String toAddress = order.getCustomer().getEmail();
+		String subject = emailSettings.getOrderConfirmationSubject();
+		String content = emailSettings.getOrderConfirmationContent();
+		
+		subject = subject.replace("[[orderId]]", String.valueOf(order.getId()));
+		
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message);
+		
+		helper.setFrom(emailSettings.getFromAddress(), emailSettings.getSenderName());
+		helper.setTo(toAddress);
+		helper.setSubject(subject);
+		
+		DateFormat dateFormatter =  new SimpleDateFormat("HH:mm:ss E, dd MMM yyyy");
+		String orderTime = dateFormatter.format(order.getOrderTime());
+		
+		CurrencySettingBag currencySettings = settingService.getCurrencySettings();
+		
+		String totalAmount = Utility.formatCurrency(order.getTotal(), currencySettings);
+		
+		content = content.replace("[[name]]", order.getCustomer().getFullName());
+		content = content.replace("[[orderId]]", String.valueOf(order.getId()));
+		content = content.replace("[[orderTime]]", orderTime);
+		content = content.replace("[[shippingAddress]]", order.getShippingAddress());
+		content = content.replace("[[total]]", totalAmount);
+		content = content.replace("[[paymentMethod]]", order.getPaymentMethod().toString());
+		
+		helper.setText(content, true);
+		mailSender.send(message);		
+	}
+	
+	@PostMapping("/process_paypal_order")
+	public String processPayPalOrder(HttpServletRequest request, Model model) 
+			throws UnsupportedEncodingException, MessagingException {
+		String orderId = request.getParameter("orderId");
+		
+		String pageTitle = "Checkout Failure";
+		String message = null;
+		
+		try {
+			if (paypalService.validateOrder(orderId)) {
+				return placeOrder(request);
+			} else {
+				pageTitle = "Checkout Failure";
+				message = "ERROR: Transaction could not be completed because order information is invalid";
+			}
+		} catch (PayPalApiException e) {
+			message = "ERROR: Transaction failed due to error: " + e.getMessage();
+		}
+		
+		model.addAttribute("pageTitle", pageTitle);
+		model.addAttribute("title", pageTitle);
+		model.addAttribute("message", message);
+		
+		return "message";
+	}
+	
 }
