865def0eff1c05027d9a73f5a153b89b587ad9cf
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-27 12:38:24 +0000

    One Thousand Three Hundred Seventy Two Commit: Implement prepareCheckout() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/CheckoutService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/CheckoutService.java
@@ -1,5 +1,35 @@
 package com.shopme.checkout;
 
-public class CheckoutService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.shopme.common.entity.CartItem;
+import com.shopme.common.entity.ShippingRate;
+import com.shopme.common.entity.product.Product;
+
+@Service
+public class CheckoutService {
+	
+	private static final int DIM_DIVISOR = 139;
+
+	public CheckoutInfo prepareCheckout(List<CartItem> cartItems, ShippingRate shippingRate) {
+		CheckoutInfo checkoutInfo = new CheckoutInfo();
+		
+		float productCost = calculateProductCost(cartItems);
+		float productTotal = calculateProductTotal(cartItems);
+		float shippingCostTotal = calculateShippingCost(cartItems, shippingRate);
+		float paymentTotal = productTotal + shippingCostTotal;
+		
+		checkoutInfo.setProductCost(productCost);
+		checkoutInfo.setProductTotal(productTotal);
+		checkoutInfo.setShippingCostTotal(shippingCostTotal);
+		checkoutInfo.setPaymentTotal(paymentTotal);
+		
+		checkoutInfo.setDeliverDays(shippingRate.getDays());
+		checkoutInfo.setCodSupported(shippingRate.isCodSupported());
+		
+		return checkoutInfo;
+	}
+	
 }
