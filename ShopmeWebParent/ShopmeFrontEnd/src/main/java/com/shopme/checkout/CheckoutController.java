2322fe56dcaf5bf4714ea6084cbcd95ba2b6674e
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-27 14:10:23 +0000

    One Thousand Three Hundred Ninety Seven Commit: Implement showCheckoutPage() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/CheckoutController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/checkout/CheckoutController.java
@@ -1,5 +1,94 @@
 package com.shopme.checkout;
 
-public class CheckoutController {
+import java.io.UnsupportedEncodingException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.shopme.Utility;
+import com.shopme.address.AddressService;
+import com.shopme.checkout.paypal.PayPalApiException;
+import com.shopme.checkout.paypal.PayPalService;
+import com.shopme.common.entity.Address;
+import com.shopme.common.entity.CartItem;
+import com.shopme.common.entity.Customer;
+import com.shopme.common.entity.ShippingRate;
+import com.shopme.common.entity.order.Order;
+import com.shopme.common.entity.order.PaymentMethod;
+import com.shopme.customer.CustomerService;
+import com.shopme.order.OrderService;
+import com.shopme.setting.CurrencySettingBag;
+import com.shopme.setting.EmailSettingBag;
+import com.shopme.setting.PaymentSettingBag;
+import com.shopme.setting.SettingService;
+import com.shopme.shipping.ShippingRateService;
+import com.shopme.shoppingcart.ShoppingCartService;
+
+@Controller
+public class CheckoutController {
+	
+	@Autowired 
+	private CheckoutService checkoutService;
+	@Autowired 
+	private CustomerService customerService;
+	@Autowired 
+	private AddressService addressService;
+	@Autowired 
+	private ShippingRateService shipService;
+	@Autowired 
+	private ShoppingCartService cartService;
+	@Autowired 
+	private OrderService orderService;
+	@Autowired 
+	private SettingService settingService;
+	@Autowired 
+	private PayPalService paypalService;
+	
+	@GetMapping("/checkout")
+	public String showCheckoutPage(Model model, HttpServletRequest request) {
+		Customer customer = getAuthenticatedCustomer(request);
+		
+		Address defaultAddress = addressService.getDefaultAddress(customer);
+		ShippingRate shippingRate = null;
+		
+		if (defaultAddress != null) {
+			model.addAttribute("shippingAddress", defaultAddress.toString());
+			shippingRate = shipService.getShippingRateForAddress(defaultAddress);
+		} else {
+			model.addAttribute("shippingAddress", customer.toString());
+			shippingRate = shipService.getShippingRateForCustomer(customer);
+		}
+		
+		if (shippingRate == null) {
+			return "redirect:/cart";
+		}
+		
+		List<CartItem> cartItems = cartService.listCartItems(customer);
+		CheckoutInfo checkoutInfo = checkoutService.prepareCheckout(cartItems, shippingRate);
+		
+		String currencyCode = settingService.getCurrencyCode();
+		PaymentSettingBag paymentSettings = settingService.getPaymentSettings();
+		String paypalClientId = paymentSettings.getClientID();
+		
+		model.addAttribute("paypalClientId", paypalClientId);
+		model.addAttribute("currencyCode", currencyCode);
+		model.addAttribute("customer", customer);
+		model.addAttribute("checkoutInfo", checkoutInfo);
+		model.addAttribute("cartItems", cartItems);
+		
+		return "checkout/checkout";
+	}
+	
 }
