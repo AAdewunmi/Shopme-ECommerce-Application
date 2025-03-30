7c6e89a08cfba0e4af85fee1f75aa38280e92e62
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-03-30 08:38:07 +0100

    One Thousand Four Hundred Ten Commit: Implement getPaymentSettings() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/SettingsService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/setting/SettingsService.java
@@ -45,4 +45,8 @@ public class SettingsService {
 	public List<Setting> getCurrencySettings() {
 		return repo.findByCategory(SettingCategory.CURRENCY);
 	}
+	
+	public List<Setting> getPaymentSettings() {
+		return repo.findByCategory(SettingCategory.PAYMENT);
+	}
 }
