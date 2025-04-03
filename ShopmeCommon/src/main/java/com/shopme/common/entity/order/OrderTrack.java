25fe05842d754c03a99c07c9c4cb24ca848ee677
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-03 08:19:49 +0100

    One Thousand Four Hundred Twenty Four Commit: Implement OrderTrack entity class

--- ShopmeCommon/src/main/java/com/shopme/common/entity/order/OrderTrack.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/order/OrderTrack.java
@@ -1,5 +1,70 @@
 package com.shopme.common.entity.order;
 
-public class OrderTrack {
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+import com.shopme.common.entity.IdBasedEntity;
+
+@Entity
+@Table(name = "order_track")
+public class OrderTrack {
+	
+	@Column(length = 256)
+	private String notes;
+	
+	private Date updatedTime;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(length = 45, nullable = false)
+	private OrderStatus status;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public Date getUpdatedTime() {
+		return updatedTime;
+	}
+
+	public void setUpdatedTime(Date updatedTime) {
+		this.updatedTime = updatedTime;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	
+	
+	
 }
