5cbb0c28a495a21d20f8ec97144e14af03ab2d10
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-05 08:32:06 +0100

    One Thousand Four Hundred Forty Three Commit: Implement JS ready() function

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_overview_products.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_overview_products.js
@@ -1,3 +1,35 @@
-/**
- * 
- */
+var fieldProductCost;
+var fieldSubtotal;
+var fieldShippingCost;
+var fieldTax;
+var fieldTotal;
+
+$(document).ready(function() {
+	
+	fieldProductCost = $("#productCost");
+	fieldSubtotal = $("#subtotal");
+	fieldShippingCost = $("#shippingCost");
+	fieldTax = $("#tax");
+	fieldTotal = $("#total");
+	
+	formatOrderAmounts();
+	formatProductAmounts();
+	
+	$("#productList").on("change", ".quantity-input", function(e) {
+		updateSubtotalWhenQuantityChanged($(this));
+		updateOrderAmounts();
+	});
+	
+	$("#productList").on("change", ".price-input", function(e) {
+		updateSubtotalWhenPriceChanged($(this));
+		updateOrderAmounts();
+	});	
+	
+	$("#productList").on("change", ".cost-input", function(e) {
+		updateOrderAmounts();
+	});
+	
+	$("#productList").on("change", ".ship-input", function(e) {
+		updateOrderAmounts();
+	});			
+});
