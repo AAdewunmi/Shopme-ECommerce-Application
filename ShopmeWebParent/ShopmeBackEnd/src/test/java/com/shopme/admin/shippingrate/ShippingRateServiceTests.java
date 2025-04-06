8c9203065d2435aad269ff32b3adad3f97c7e55b
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-06 07:59:33 +0100

    One Thousand Four Hundred Fifty Seven Commit: Implement testCalculateShippingCost_NoRateFound() method

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/shippingrate/ShippingRateServiceTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/shippingrate/ShippingRateServiceTests.java
@@ -1,5 +1,50 @@
 package com.shopme.admin.shippingrate;
 
-public class ShippingRateServiceTests {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.shopme.admin.product.ProductRepository;
+import com.shopme.common.entity.ShippingRate;
+import com.shopme.common.entity.product.Product;
+
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+public class ShippingRateServiceTests {
+	
+	@MockBean 
+	private ShippingRateRepository shipRepo;
+	
+	@MockBean 
+	private ProductRepository productRepo;
+	
+	@InjectMocks
+	private ShippingRateService shipService;
+	
+	@Test
+	public void testCalculateShippingCost_NoRateFound() {
+		Integer productId = 1;
+		Integer countryId = 234;
+		String state = "ABCDE";
+		
+		Mockito.when(shipRepo.findByCountryAndState(countryId, state)).thenReturn(null);
+		
+		assertThrows(ShippingRateNotFoundException.class, new Executable() {
+			
+			@Override
+			public void execute() throws Throwable {
+				shipService.calculateShippingCost(productId, countryId, state);
+			}
+		});
+	}
 }
