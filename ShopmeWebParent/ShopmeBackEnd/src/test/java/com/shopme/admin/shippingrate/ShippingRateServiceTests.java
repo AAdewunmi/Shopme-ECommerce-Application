298387058164c27c78665cb428b940f79a83c2a1
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-06 08:02:24 +0100

    One Thousand Four Hundred Fifty Eight Commit: Implement testCalculateShippingCost_RateFound() method

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/shippingrate/ShippingRateServiceTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/shippingrate/ShippingRateServiceTests.java
@@ -47,4 +47,28 @@ public class ShippingRateServiceTests {
 			}
 		});
 	}
+	
+	@Test
+	public void testCalculateShippingCost_RateFound() throws ShippingRateNotFoundException {
+		Integer productId = 1;
+		Integer countryId = 234;
+		String state = "New York";
+		
+		ShippingRate shippingRate = new ShippingRate();
+		shippingRate.setRate(10);
+		
+		Mockito.when(shipRepo.findByCountryAndState(countryId, state)).thenReturn(shippingRate);
+		
+		Product product = new Product();
+		product.setWeight(5);
+		product.setWidth(4);
+		product.setHeight(3);
+		product.setLength(8);		
+		
+		Mockito.when(productRepo.findById(productId)).thenReturn(Optional.of(product));
+	
+		float shippingCost = shipService.calculateShippingCost(productId, countryId, state);
+		
+		assertEquals(50, shippingCost);
+	}
 }
