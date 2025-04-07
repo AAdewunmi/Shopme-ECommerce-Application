5b75338a0276406fe65ca548604b949da34a84b0
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-07 13:46:05 +0100

    One Thousand Four Hundred Seventy Two Commit: Implement addProduct() JS function

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_add_product.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_add_product.js
@@ -1,3 +1,21 @@
-/**
- * 
- */
+var productDetailCount;
+
+$(document).ready(function() {
+	productDetailCount = $(".hiddenProductId").length;
+	
+	$("#products").on("click", "#linkAddProduct", function(e) {
+		e.preventDefault();
+		link = $(this);
+		url = link.attr("href");
+		
+		$("#addProductModal").on("shown.bs.modal", function() {
+			$(this).find("iframe").attr("src", url);
+		});
+		
+		$("#addProductModal").modal();
+	})
+});
+
+function addProduct(productId, productName) {
+	getShippingCost(productId);	
+}
