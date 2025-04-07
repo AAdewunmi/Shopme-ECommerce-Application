f9d3667ca1c25c04b628240e5aa40ce860259120
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-07 13:50:59 +0100

    One Thousand Four Hundred Seventy Five Commit: Implement generateProductCode() JS function

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_add_product.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_add_product.js
@@ -68,3 +68,88 @@ function getProductInfo(productId, shippingCost) {
 		showWarningModal(err.responseJSON.message);
 	});	
 }
+
+function generateProductCode(productId, productName, mainImagePath, productCost, productPrice, shippingCost) {
+	nextCount = productDetailCount + 1;
+	productDetailCount++;
+	rowId = "row" + nextCount;
+	quantityId = "quantity" + nextCount;
+	priceId = "price" + nextCount;
+	subtotalId = "subtotal" + nextCount;
+	blankLineId= "blankLine" + nextCount;
+	
+	htmlCode = `
+		<div class="border rounded p-1" id="${rowId}">
+			<input type="hidden" name="detailId" value="0" />
+			<input type="hidden" name="productId" value="${productId}" class="hiddenProductId" />
+			<div class="row">
+				<div class="col-1">
+					<div class="divCount">${nextCount}</div>
+					<div><a class="fas fa-trash icon-dark linkRemove" href="" rowNumber="${nextCount}"></a></div>				
+				</div>
+				<div class="col-3">
+					<img src="${mainImagePath}" class="img-fluid" />
+				</div>
+			</div>
+			
+			<div class="row m-2">
+				<b>${productName}</b>
+			</div>
+			
+			<div class="row m-2">
+			<table>
+				<tr>
+					<td>Product Cost:</td>
+					<td>
+						<input type="text" required class="form-control m-1 cost-input"
+							name="productDetailCost"
+							rowNumber="${nextCount}" 
+							value="${productCost}" style="max-width: 140px"/>
+					</td>
+				</tr>
+				<tr>
+					<td>Quantity:</td>
+					<td>
+						<input type="number" step="1" min="1" max="5" class="form-control m-1 quantity-input"
+							name="quantity"
+							id="${quantityId}"
+							rowNumber="${nextCount}" 
+							value="1" style="max-width: 140px"/>
+					</td>
+				</tr>	
+				<tr>
+					<td>Unit Price:</td>
+					<td>
+						<input type="text" required class="form-control m-1 price-input"
+							name="productPrice"
+							id="${priceId}"
+							rowNumber="${nextCount}" 
+							value="${productPrice}" style="max-width: 140px"/>
+					</td>
+				</tr>
+				<tr>
+					<td>Subtotal:</td>
+					<td>
+						<input type="text" readonly="readonly" class="form-control m-1 subtotal-output"
+							name="productSubtotal"
+							id="${subtotalId}" 
+							value="${productPrice}" style="max-width: 140px"/>
+					</td>
+				</tr>				
+				<tr>
+					<td>Shipping Cost:</td>
+					<td>
+						<input type="text" required class="form-control m-1 ship-input"
+							name="productShipCost" 
+							value="${shippingCost}" style="max-width: 140px"/>
+					</td>
+				</tr>											
+			</table>
+			</div>
+			
+		</div>
+		<div id="${blankLineId}"class="row">&nbsp;</div>	
+	`;	
+	
+	return htmlCode;
+}
