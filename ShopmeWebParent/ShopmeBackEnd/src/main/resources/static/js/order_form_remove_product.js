50589ec5395496c293db4236d209c67e47ad7124
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-09 07:20:04 +0100

    One Thousand Four Hundred Eighty One Commit: Implement ready() JS function

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_remove_product.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_remove_product.js
@@ -1,3 +1,12 @@
-/**
- * 
- */
+$(document).ready(function() {
+	$("#productList").on("click", ".linkRemove", function(e) {
+		e.preventDefault();
+		
+		if (doesOrderHaveOnlyOneProduct()) {
+			showWarningModal("Could not remove product. The order must have eat least one product.");
+		} else {
+			removeProduct($(this));		
+			updateOrderAmounts();	
+		}
+	});
+});
