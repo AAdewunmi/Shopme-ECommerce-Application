853de7040a0ccfe6a2dc82237e01b9bd003ab711
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-10 09:01:22 +0100

    One Thousand Four Hundred Ninety Two Commit: Implement formatCurrentDateTime() JS function

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_tracks.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_tracks.js
@@ -91,3 +91,23 @@ function generateTrackCode() {
 	
 	return htmlCode;
 }
+
+function formatCurrentDateTime() {
+	date = new Date();
+	year = date.getFullYear();
+	month = date.getMonth() + 1;
+	day = date.getDate();
+	hour = date.getHours();
+	minute = date.getMinutes();
+	second = date.getSeconds();
+	
+	if (month < 10) month = "0" + month;
+	if (day < 10) day = "0" + day;
+	
+	if (hour < 10) hour = "0" + hour;
+	if (minute < 10) minute = "0" + minute;
+	if (second < 10) second = "0" + second;
+	
+	return year + "-" + month + "-" + day + "T" + hour + ":" + minute + ":" + second;
+	
+}
