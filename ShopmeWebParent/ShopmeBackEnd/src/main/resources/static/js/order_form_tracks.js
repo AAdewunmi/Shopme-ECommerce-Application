017335e57f263dbe85e256579db98588635927c9
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-10 08:58:00 +0100

    One Thousand Four Hundred Ninety One Commit: Implement generateTrackCode() JS method

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_tracks.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_tracks.js
@@ -40,3 +40,54 @@ function addNewTrackRecord() {
 	htmlCode = generateTrackCode();	
 	$("#trackList").append(htmlCode);
 }
+
+function generateTrackCode() {
+	nextCount = trackRecordCount + 1;
+	trackRecordCount++;
+	rowId = "rowTrack" + nextCount;
+	emptyLineId = "emptyLine" + nextCount;
+	trackNoteId = "trackNote" + nextCount;
+	currentDateTime = formatCurrentDateTime();
+	
+	htmlCode = `
+			<div class="row border rounded p-1" id="${rowId}">
+				<input type="hidden" name="trackId" value="0" class="hiddenTrackId" />
+				<div class="col-2">
+					<div class="divCountTrack">${nextCount}</div>
+					<div class="mt-1"><a class="fas fa-trash icon-dark linkRemoveTrack" href="" rowNumber="${nextCount}"></a></div>					
+				</div>				
+				
+				<div class="col-10">
+				  <div class="form-group row">
+				    <label class="col-form-label">Time:</label>
+				    <div class="col">
+						<input type="datetime-local" name="trackDate" value="${currentDateTime}" class="form-control" required
+							style="max-width: 300px"/>						
+				    </div>
+				  </div>					
+				<div class="form-group row">  
+				<label class="col-form-label">Status:</label>
+				<div class="col">
+					<select name="trackStatus" class="form-control dropDownStatus" required style="max-width: 150px" rowNumber="${nextCount}">
+			`;
+	  
+	htmlCode += $("#trackStatusOptions").clone().html();
+	
+	htmlCode += `
+				      </select>						
+				    </div>
+				  </div>
+				  <div class="form-group row">
+				    <label class="col-form-label">Notes:</label>
+				    <div class="col">
+						<textarea rows="2" cols="10" class="form-control" name="trackNotes" id="${trackNoteId}" style="max-width: 300px" required></textarea>
+				    </div>
+				  </div>
+				  
+				</div>				
+			</div>	
+			<div id="${emptyLineId}" class="row">&nbsp;</div>
+	`;
+	
+	return htmlCode;
+}
