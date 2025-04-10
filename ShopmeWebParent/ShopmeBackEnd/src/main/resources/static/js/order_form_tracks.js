3116c9065eb70f4cf7332dd9d7e6d24cd6dd8015
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-10 08:25:27 +0100

    One Thousand Four Hundred Eighty Seven Commit: Implement ready() JS function

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_tracks.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/order_form_tracks.js
@@ -1,3 +1,25 @@
-/**
- * 
- */
+var trackRecordCount;
+
+$(document).ready(function() {
+	trackRecordCount = $(".hiddenTrackId").length;
+	
+	$("#trackList").on("click", ".linkRemoveTrack", function(e) {
+		e.preventDefault();
+		deleteTrack($(this));
+		updateTrackCountNumbers();
+	});
+	
+	$("#track").on("click", "#linkAddTrack", function(e) {
+		e.preventDefault();
+		addNewTrackRecord();
+	});
+	
+	$("#trackList").on("change", ".dropDownStatus", function(e) {
+		dropDownList = $(this);
+		rowNumber = dropDownList.attr("rowNumber");
+		selectedOption = $("option:selected", dropDownList);
+		
+		defaultNote = selectedOption.attr("defaultDescription");
+		$("#trackNote" + rowNumber).text(defaultNote);
+	});	
+});
