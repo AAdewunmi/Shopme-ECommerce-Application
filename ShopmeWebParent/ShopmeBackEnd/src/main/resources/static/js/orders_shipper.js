a306da1ce4a65d2e784db34949225598799bb7d4
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-13 09:51:41 +0100

    One Thousand Five Hundred Four Commit: Implement ready() JS function

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/orders_shipper.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/orders_shipper.js
@@ -1,3 +1,26 @@
-/**
- * 
- */
+var iconNames = {
+	'PICKED':'fa-people-carry',
+	'SHIPPING':'fa-shipping-fast',
+	'DELIVERED':'fa-box-open',
+	'RETURNED':'fa-undo'	
+};
+
+var confirmText;  
+var confirmModalDialog;
+var yesButton;
+var noButton;
+
+$(document).ready(function() {
+	confirmText = $("#confirmText");
+	confirmModalDialog = $("#confirmModal");
+	yesButton = $("#yesButton");
+	noButton = $("#noButton");
+	
+	$(".linkUpdateStatus").on("click", function(e) {
+		e.preventDefault();
+		link = $(this);
+		showUpdateConfirmModal(link);
+	});
+	
+	addEventHandlerForYesButton();
+});
