cfb59f3836e18368fcba60ed7efbc5ec987de466
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-14 07:49:40 +0100

    One Thousand Five Hundred Eighteen Commit: Implement updateOrderStatus() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderRestController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderRestController.java
@@ -1,5 +1,20 @@
 package com.shopme.admin.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OrderRestController {
+	
+	@Autowired
+	private OrderService service;
+
+	@PostMapping("/orders_shipper/update/{id}/{status}")
+	public Response updateOrderStatus(@PathVariable("id") Integer orderId, @PathVariable("status") String status) {
+		service.updateStatus(orderId, status);
+		return new Response(orderId, status);
+	}
 
 }
