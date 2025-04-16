442f1c8ac5a9f6aecfd8eb19b7585a0686b47beb
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-16 09:37:45 +0100

    One Thousand Five Hundred Thirty One Commit: Implement listFirstPage() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/order/OrderController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/order/OrderController.java
@@ -1,5 +1,32 @@
 package com.shopme.order;
 
+import java.util.List;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.shopme.Utility;
+import com.shopme.common.entity.Customer;
+import com.shopme.common.entity.order.Order;
+import com.shopme.customer.CustomerService;
+
+@Controller
 public class OrderController {
+	
+	@Autowired 
+	private OrderService orderService;
+	@Autowired 
+	private CustomerService customerService;
+	
+	@GetMapping("/orders")
+	public String listFirstPage(Model model, HttpServletRequest request) {
+		return listOrdersByPage(model, request, 1, "orderTime", "desc", null);
+	}
 
 }
