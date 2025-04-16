a4cb16b6460cf583627691bf6fd9a559d424b017
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-16 08:39:22 +0100

    One Thousand Five Hundred Twenty Seven Commit: Implement getProductNames() method

--- ShopmeCommon/src/main/java/com/shopme/common/entity/order/Order.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/order/Order.java
@@ -308,4 +308,19 @@ public class Order extends AbstractAddress {
 		return false;
 	}
 	
+	@Transient
+	public String getProductNames() {
+		String productNames = "";
+		
+		productNames = "<ul>";
+		
+		for (OrderDetail detail : orderDetails) {
+			productNames += "<li>" + detail.getProduct().getShortName() + "</li>";			
+		}
+		
+		productNames += "</ul>";
+		
+		return productNames;
+	}	
+	
 }
