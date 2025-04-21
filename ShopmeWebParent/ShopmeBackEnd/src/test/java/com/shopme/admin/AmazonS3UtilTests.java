e49c39dee9302a79c0465af3594c74dd87335400
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-21 08:54:49 +0100

    One Thousand Five Hundred Eighty Commit: Implement testListFolder() method

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/AmazonS3UtilTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/AmazonS3UtilTests.java
@@ -1,5 +1,19 @@
 package com.shopme.admin;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
 public class AmazonS3UtilTests {
+	
+	@Test
+	public void testListFolder() {
+		String folderName = "product-images/10/";
+		// List<String> listKeys = AmazonS3Util.listFolder(folderName);
+		//listKeys.forEach(System.out::println);
+	}
 
 }
