1a88ed4a0407e06c22b3b3a6663c1cda7fc32b01
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-22 15:50:48 +0100

    One Thousand Five Hundred Ninety Four Commit: Declare findWithCategoryAndTimeBetween() abstract method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderDetailRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/order/OrderDetailRepository.java
@@ -1,5 +1,18 @@
 package com.shopme.admin.order;
 
-public interface OrderDetailRepository {
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.shopme.common.entity.order.OrderDetail;
+
+public interface OrderDetailRepository extends CrudRepository<OrderDetail, Integer>{
+	
+	@Query("SELECT NEW com.shopme.common.entity.order.OrderDetail(d.product.category.name, d.quantity,"
+			+ " d.productCost, d.shippingCost, d.subtotal)"
+			+ " FROM OrderDetail d WHERE d.order.orderTime BETWEEN ?1 AND ?2")
+	public List<OrderDetail> findWithCategoryAndTimeBetween(Date startTime, Date endTime);
 
 }
