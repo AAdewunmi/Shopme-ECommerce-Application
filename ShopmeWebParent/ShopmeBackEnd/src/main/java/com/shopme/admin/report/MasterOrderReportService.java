d4fc0dd56feb24dbf93ae86b019b9a7a784dcd9a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-23 16:58:46 +0100

    One Thousand Six Hundred Nineteen Commit: Implement createReportData() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/MasterOrderReportService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/MasterOrderReportService.java
@@ -57,5 +57,36 @@ public class MasterOrderReportService extends AbstractReportService{
 		});
 		
 	}
+	
+	private List<ReportItem> createReportData(Date startTime, Date endTime, ReportType reportType) {
+		List<ReportItem> listReportItems = new ArrayList<>();
+		
+		Calendar startDate = Calendar.getInstance();
+		startDate.setTime(startTime);
+		
+		Calendar endDate = Calendar.getInstance();
+		endDate.setTime(endTime);	
+		
+		Date currentDate = startDate.getTime();
+		String dateString = dateFormatter.format(currentDate);
+		
+		listReportItems.add(new ReportItem(dateString));
+		
+		do {
+			if (reportType.equals(ReportType.DAY)) {
+				startDate.add(Calendar.DAY_OF_MONTH, 1);
+			} else if (reportType.equals(ReportType.MONTH)) {
+				startDate.add(Calendar.MONTH, 1);
+			}
+			
+			currentDate = startDate.getTime();
+			dateString = dateFormatter.format(currentDate);	
+			
+			listReportItems.add(new ReportItem(dateString));
+			
+		} while (startDate.before(endDate));
+		
+		return listReportItems;		
+	}
 
 }
