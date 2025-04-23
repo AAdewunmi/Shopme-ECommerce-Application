2915c5346536c2a146142034ab32cc2adad30b05
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-23 16:54:44 +0100

    One Thousand Six Hundred Fifteen Commit: Implement getReportDataByDateRangeInternal() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/MasterOrderReportService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/MasterOrderReportService.java
@@ -1,5 +1,35 @@
 package com.shopme.admin.report;
 
-public class MasterOrderReportService {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shopme.admin.order.OrderRepository;
+import com.shopme.common.entity.order.Order;
+
+@Service
+public class MasterOrderReportService extends AbstractReportService{
+	
+@Autowired private OrderRepository repo;
+
+	
+	protected List<ReportItem> getReportDataByDateRangeInternal(Date startTime, Date endTime, ReportType reportType) {
+		List<Order> listOrders = repo.findByOrderTimeBetween(startTime, endTime);
+		printRawData(listOrders);
+		
+		List<ReportItem> listReportItems = createReportData(startTime, endTime, reportType);
+		
+		System.out.println();
+		
+		calculateSalesForReportData(listOrders, listReportItems);
+		
+		printReportData(listReportItems);
+		
+		return listReportItems;
+	}
 
 }
