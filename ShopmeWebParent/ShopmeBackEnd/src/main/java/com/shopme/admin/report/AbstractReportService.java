82690581fcb86a490b3d365226b53a97f9b29f9d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-23 16:31:48 +0100

    One Thousand Six Hundred Seven Commit: Implement getReportDataLast7Days() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/AbstractReportService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/AbstractReportService.java
@@ -1,5 +1,18 @@
 package com.shopme.admin.report;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 public class AbstractReportService {
 
+protected DateFormat dateFormatter;
+	
+	public List<ReportItem> getReportDataLast7Days(ReportType reportType) {
+		return getReportDataLastXDays(7, reportType);
+	}
+	
+	
 }
