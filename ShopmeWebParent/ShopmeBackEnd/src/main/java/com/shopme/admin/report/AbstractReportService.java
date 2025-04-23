d6569319acb68f5d7ef92f3f4a3104dc229433c2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-23 16:43:47 +0100

    One Thousand Six Hundred Nine Commit: Implement getReportDataLastXDays() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/AbstractReportService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/AbstractReportService.java
@@ -17,4 +17,20 @@ protected DateFormat dateFormatter;
 	public List<ReportItem> getReportDataLast28Days(ReportType reportType) {
 		return getReportDataLastXDays(28, reportType);
 	}
+	
+	protected List<ReportItem> getReportDataLastXDays(int days, ReportType reportType) {
+		Date endTime = new Date();
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DAY_OF_MONTH, -(days - 1));
+		Date startTime = cal.getTime();
+		
+		System.out.println("Start time: " + startTime);
+		System.out.println("End time: " + endTime);
+		
+		dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+		
+		return getReportDataByDateRangeInternal(startTime, endTime, reportType);
+	}
+	
+	
 }
