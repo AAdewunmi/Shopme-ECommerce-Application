d140a3e58b5c25f31214ace05f509a3c7dfa54b9
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-24 15:44:38 +0100

    One Thousand Six Hundred Thirty Six Commit: Implement viewSalesReportHome() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/ReportController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/report/ReportController.java
@@ -1,5 +1,25 @@
 package com.shopme.admin.report;
 
+import java.util.List;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.shopme.admin.setting.SettingsService;
+import com.shopme.common.entity.setting.Setting;
+
+@Controller
 public class ReportController {
+	
+@Autowired private SettingsService settingService;
+	
+	@GetMapping("/reports")
+	public String viewSalesReportHome(HttpServletRequest request) {
+		loadCurrencySetting(request);
+		return "reports/reports";
+	}
 
 }
