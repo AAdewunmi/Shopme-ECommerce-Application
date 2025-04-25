17928a1fdeabe65f5f98eea060d9d9d7e13b3f6f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-25 15:02:55 +0100

    One Thousand Six Hundred Forty Eight Commit: Implement loadSalesReportByDateForProduct() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/sales_report_by_product.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/sales_report_by_product.js
@@ -1,113 +1,60 @@
-// Sales Report Common
-var MILLISECONDS_A_DAY = 24 * 60 * 60 * 1000;
+// Sales Report by Product
+var data;
+var chartOptions;
 
-function setupButtonEventHandlers(reportType, callbackFunction) {
-	$(".button-sales-by" + reportType).on("click", function() {
-		$(".button-sales-by" + reportType).each(function(e) {
-			$(this).removeClass('btn-primary').addClass('btn-light');
-		});
-		
-		$(this).removeClass('btn-light').addClass('btn-primary');
-		
-		period = $(this).attr("period");
-		if (period) {
-			callbackFunction(period);
-			$("#divCustomDateRange" + reportType).addClass("d-none");
-		} else {
-			$("#divCustomDateRange" + reportType).removeClass("d-none");
-		}		
-	});
-	
-	initCustomDateRange(reportType);
-	
-	$("#buttonViewReportByDateRange" + reportType).on("click", function(e) {
-		validateDateRange(reportType, callbackFunction);
-	});	
-}
+$(document).ready(function() {
+	setupButtonEventHandlers("_product", loadSalesReportByDateForProduct);	
+});
 
-function validateDateRange(reportType, callbackFunction) {
-	startDateField = document.getElementById('startDate' + reportType);
-	days = calculateDays(reportType);
-	
-	startDateField.setCustomValidity("");
-	
-	if (days >= 7 && days <= 30) {
-		callbackFunction("custom");
+function loadSalesReportByDateForProduct(period) {
+	if (period == "custom") {
+		startDate = $("#startDate_product").val();
+		endDate = $("#endDate_product").val();
+		
+		requestURL = contextPath + "reports/product/" + startDate + "/" + endDate;
 	} else {
-		startDateField.setCustomValidity("Dates must be in the range of 7..30 days");
-		startDateField.reportValidity();
+		requestURL = contextPath + "reports/product/" + period;		
 	}
-}
-
-function calculateDays(reportType) {
-	startDateField = document.getElementById('startDate' + reportType);
-	endDateField = document.getElementById('endDate' + reportType);
 	
-	startDate = startDateField.valueAsDate;
-	endDate = endDateField.valueAsDate;
-	
-	differenceInMilliseconds = endDate - startDate;
-	return differenceInMilliseconds / MILLISECONDS_A_DAY;
-}
-	
-function initCustomDateRange(reportType) {
-	startDateField = document.getElementById('startDate' + reportType);
-	endDateField = document.getElementById('endDate' + reportType);
-		
-	toDate = new Date();
-	endDateField.valueAsDate = toDate;
-	
-	fromDate = new Date();
-	fromDate.setDate(toDate.getDate() - 30);
-	startDateField.valueAsDate = fromDate;
-}	
-
-function formatCurrency(amount) {
-	formattedAmount = $.number(amount, decimalDigits, decimalPointType, thousandsPointType);
-	return prefixCurrencySymbol + formattedAmount + suffixCurrencySymbol;
-}
-
-function getChartTitle(period) {
-	if (period == "last_7_days") return "Sales in Last 7 Days";
-	if (period == "last_28_days") return "Sales in Last 28 Days";
-	if (period == "last_6_months") return "Sales in Last 6 Months";
-	if (period == "last_year") return "Sales in Last Year";
-	if (period == "custom") return "Custom Date Range";
-	
-	return "";
-}
-
-function getDenominator(period, reportType) {
-	if (period == "last_7_days") return 7;
-	if (period == "last_28_days") return 28;
-	if (period == "last_6_months") return 6;
-	if (period == "last_year") return 12;
-	if (period == "custom") return calculateDays(reportType);
-	
-	return 7;
-}
-
-function setSalesAmount(period, reportType, labelTotalItems) {
-	$("#textTotalGrossSales" + reportType).text(formatCurrency(totalGrossSales));
-	$("#textTotalNetSales" + reportType).text(formatCurrency(totalNetSales));
-	
-	denominator = getDenominator(period, reportType);
-	
-	$("#textAvgGrossSales" + reportType).text(formatCurrency(totalGrossSales / denominator));
-	$("#textAvgNetSales" + reportType).text(formatCurrency(totalNetSales / denominator));
-	$("#labelTotalItems" + reportType).text(labelTotalItems);
-	$("#textTotalItems" + reportType).text(totalItems);	
-}
-
-function formatChartData(data, columnIndex1, columnIndex2) {
-	var formatter = new google.visualization.NumberFormat({
-		prefix: prefixCurrencySymbol,
-		suffix: suffixCurrencySymbol,
-		decimalSymbol: decimalPointType,
-		groupingSymbol: thousandsPointType,
-		fractionDigits: decimalDigits
+	$.get(requestURL, function(responseJSON) {
+		prepareChartDataForSalesReportByProduct(responseJSON);
+		customizeChartForSalesReportByProduct();
+		formatChartData(data, 2, 3);
+		drawChartForSalesReportByProduct(period);
+		setSalesAmount(period, '_product', "Total Products");
 	});
+}
+
+function prepareChartDataForSalesReportByProduct(responseJSON) {
+	data = new google.visualization.DataTable();
+	data.addColumn('string', 'Product');
+	data.addColumn('number', 'Quantity');
+	data.addColumn('number', 'Gross Sales');
+	data.addColumn('number', 'Net Sales');
 	
-	formatter.format(data, columnIndex1);
-	formatter.format(data, columnIndex2);	
+	totalGrossSales = 0.0;
+	totalNetSales = 0.0;
+	totalItems = 0;
+	
+	$.each(responseJSON, function(index, reportItem) {
+		data.addRows([[reportItem.identifier, reportItem.productsCount, reportItem.grossSales, reportItem.netSales]]);
+		totalGrossSales += parseFloat(reportItem.grossSales);
+		totalNetSales += parseFloat(reportItem.netSales);
+		totalItems += parseInt(reportItem.productsCount);
+	});
+}
+
+function customizeChartForSalesReportByProduct() {
+	chartOptions = {
+		height: 360, width: '80%',
+		showRowNumber: true,
+		page: 'enable',
+		sortColumn: 2,
+		sortAscending: false
+	};
+}
+
+function drawChartForSalesReportByProduct() {
+	var salesChart = new google.visualization.Table(document.getElementById('chart_sales_by_product'));
+	salesChart.draw(data, chartOptions);
 }
