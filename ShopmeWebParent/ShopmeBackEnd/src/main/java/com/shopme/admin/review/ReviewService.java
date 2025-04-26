828c1e58b9e4177c060fea7a35a31c4864d18329
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-26 14:17:36 +0100

    One Thousand Six Hundred Sixty Four Commit: Implement listByPage() method

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/review/ReviewService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/review/ReviewService.java
@@ -1,5 +1,28 @@
 package com.shopme.admin.review;
 
+import java.util.NoSuchElementException;
+
+import jakarta.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shopme.admin.paging.PagingAndSortingHelper;
+import com.shopme.admin.product.ProductRepository;
+import com.shopme.common.entity.Review;
+import com.shopme.common.exception.ReviewNotFoundException;
+
+@Service
+@Transactional
 public class ReviewService {
+	
+	public static final int REVIEWS_PER_PAGE = 5;
+	
+	@Autowired private ReviewRepository reviewRepo;
+	@Autowired private ProductRepository productRepo;
+	
+	public void listByPage(int pageNum, PagingAndSortingHelper helper) {
+		helper.listEntities(pageNum, REVIEWS_PER_PAGE, reviewRepo);
+	}
 
 }
