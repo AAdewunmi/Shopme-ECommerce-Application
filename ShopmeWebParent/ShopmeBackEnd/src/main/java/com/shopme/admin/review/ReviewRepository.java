cbab71ccd9174a0ba31118c392beb5e4be85525f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-26 12:57:53 +0100

    One Thousand Six Hundred Fifty Two Commit: Create ReviewRepository interface

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/review/ReviewRepository.java
@@ -0,0 +1,8 @@
+package com.shopme.admin.review;
+
+import com.shopme.admin.paging.SearchRepository;
+import com.shopme.common.entity.Review;
+
+public interface ReviewRepository extends SearchRepository<Review, Integer> {
+
+}
