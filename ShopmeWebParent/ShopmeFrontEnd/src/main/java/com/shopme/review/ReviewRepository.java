3d67a07411c018f554dc7de8198ce6511d5a0036
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-27 08:31:24 +0100

    One Thousand Six Hundred Seventy Four Commit: Declare findByCustomer() abstract method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/review/ReviewRepository.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/review/ReviewRepository.java
@@ -1,5 +1,15 @@
 package com.shopme.review;
 
-public interface ReviewRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.shopme.common.entity.Review;
+
+public interface ReviewRepository extends JpaRepository<Review, Integer>{
+	
+	@Query("SELECT r FROM Review r WHERE r.customer.id = ?1")
+	public Page<Review> findByCustomer(Integer customerId, Pageable pageable);
 
 }
