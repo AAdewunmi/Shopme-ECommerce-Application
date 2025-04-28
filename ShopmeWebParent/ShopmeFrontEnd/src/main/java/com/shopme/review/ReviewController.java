fa48d68d0b7b00d6e6fdc930b547015f5d15136c
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-28 09:53:26 +0100

    One Thousand Six Hundred Seventy Seven Commit: Implement listFirstPage() method

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/review/ReviewController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/review/ReviewController.java
@@ -1,5 +1,39 @@
 package com.shopme.review;
 
+import java.util.List;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.shopme.ControllerHelper;
+import com.shopme.common.entity.Customer;
+import com.shopme.common.entity.Review;
+import com.shopme.common.entity.product.Product;
+import com.shopme.common.exception.ProductNotFoundException;
+import com.shopme.common.exception.ReviewNotFoundException;
+import com.shopme.product.ProductService;
+
+@Controller
 public class ReviewController {
+	
+	private 
+	String defaultRedirectURL = "redirect:/reviews/page/1?sortField=reviewTime&sortDir=desc";
+	
+	@Autowired private ReviewService reviewService;
+	@Autowired private ControllerHelper controllerHelper;
+	@Autowired private ProductService productService;
+	
+	@GetMapping("/reviews")
+	public String listFirstPage(Model model) {
+		return defaultRedirectURL;
+	}
 
 }
