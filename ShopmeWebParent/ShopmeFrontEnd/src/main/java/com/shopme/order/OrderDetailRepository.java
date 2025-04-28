e285db1abf4a9a0f59d69b609e71a93836c442ca
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-04-28 08:42:08 +0100

    One Thousand Six Hundred Seventy Three Commit: Create skeleton OrderDetailRepository interface

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/order/OrderDetailRepository.java
@@ -0,0 +1,7 @@
+package com.shopme.order;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Integer> {
+
+}
